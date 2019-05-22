d1a590d7e0a212fad9fed01c1f833b523fb3fb4b
dangquocdung <dungthinhvn@gmail.com>
2019-05-22 14:10:58 +0700

    ádsa

--- resources/views/dungthinh/home.blade.php
+++ resources/views/dungthinh/home.blade.php
@@ -83,13 +83,8 @@
               <div class="btn-group categories-btn">
                 <button class="btn btn-secondary dropdown-toggle" data-toggle="dropdown"><i class="icon-menu text-lg"></i>&nbsp;{!! trans('frontend.product_categories_label') !!}</button>
 
-                
-
-                    
-
                     {!! $dynamic_categories_megabox !!}
 
-                
               </div>
               <form class="input-group" method="get"><span class="input-group-btn">
                   <button type="submit"><i class="icon-search"></i></button></span>
@@ -126,25 +121,17 @@
                     </div>
                   </a>
 
-                  
                 @endif
               @endif
-
-
-            
-
-
 
             <ul class="toolbar-dropdown lang-dropdown">
               <li class="px-3 pt-1 pb-2">
                 <select class="form-control form-control-sm">
 
-                    
                     @if(count(get_frontend_selected_currency_data()) >0)
                     @foreach(get_frontend_selected_currency_data() as $val)
 
                       <option value="{{ $val }}">{!! get_currency_name_by_code( $val ) !!}</option>
-
 
                     @endforeach
                     @endif
@@ -170,8 +157,15 @@
               
             </ul>
           </div>
-          <div class="toolbar-item hidden-on-mobile"><a href="product-comparison.html">
-              <div><span class="compare-icon"><i class="icon-repeat"></i><span class="count-label">3</span></span><span class="text-label">Compare</span></div></a></div>
+
+          <div class="toolbar-item hidden-on-mobile">
+            <a href="product-comparison.html">
+              <div>
+                <span class="compare-icon"><i class="icon-repeat"></i><span class="count-label">3</span></span>
+                <span class="text-label">Compare</span>
+              </div>
+            </a>
+          </div>
           <div class="toolbar-item hidden-on-mobile"><a href="account-login.html">
               <div><i class="icon-user"></i><span class="text-label">Sign In / Up</span></div></a>
             <div class="toolbar-dropdown text-center px-3">
@@ -179,41 +173,17 @@
               <p class="text-xs text-muted mb-2">New customer?&nbsp;<a href="account-login.html">Register</a></p>
             </div>
           </div>
-          <div class="toolbar-item"><a href="cart.html">
-              <div><span class="cart-icon"><i class="icon-shopping-cart"></i><span class="count-label">3   </span></span><span class="text-label">Cart</span></div></a>
-            <div class="toolbar-dropdown cart-dropdown widget-cart hidden-on-mobile">
-              <!-- Entry-->
-              <div class="entry">
-                <div class="entry-thumb"><a href="shop-single.html"><img src="img/shop/widget/04.jpg" alt="Product"></a></div>
-                <div class="entry-content">
-                  <h4 class="entry-title"><a href="shop-single.html">Canon EOS M50 Mirrorless Camera</a></h4><span class="entry-meta">1 x $910.00</span>
-                </div>
-                <div class="entry-delete"><i class="icon-x"></i></div>
+          <div class="toolbar-item">
+            <a href="cart.html">
+              <div>
+                <span class="cart-icon"><i class="icon-shopping-cart"></i>
+                  <span class="count-label">3   </span>
+                </span>
+                <span class="text-label">Cart</span>
               </div>
-              <!-- Entry-->
-              <div class="entry">
-                <div class="entry-thumb"><a href="shop-single.html"><img src="img/shop/widget/05.jpg" alt="Product"></a></div>
-                <div class="entry-content">
-                  <h4 class="entry-title"><a href="shop-single.html">Apple iPhone X 256 GB Space Gray</a></h4><span class="entry-meta">1 x $1,450.00</span>
-                </div>
-                <div class="entry-delete"><i class="icon-x"></i></div>
-              </div>
-              <!-- Entry-->
-              <div class="entry">
-                <div class="entry-thumb"><a href="shop-single.html"><img src="img/shop/widget/06.jpg" alt="Product"></a></div>
-                <div class="entry-content">
-                  <h4 class="entry-title"><a href="shop-single.html">HP LaserJet Pro Laser Printer</a></h4><span class="entry-meta">1 x $188.50</span>
-                </div>
-                <div class="entry-delete"><i class="icon-x"></i></div>
-              </div>
-              <div class="text-right">
-                <p class="text-gray-dark py-2 mb-0"><span class='text-muted'>Subtotal:</span> &nbsp;$2,548.50</p>
-              </div>
-              <div class="d-flex">
-                <div class="pr-2 w-50"><a class="btn btn-secondary btn-sm btn-block mb-0" href="cart.html">Expand Cart</a></div>
-                <div class="pl-2 w-50"><a class="btn btn-primary btn-sm btn-block mb-0" href="checkout.html">Checkout</a></div>
-              </div>
-            </div>
+            </a>
+            @include('pages.ajax-pages.mini-cart-dt')
+            
           </div>
         </div>
         <!-- Mobile Menu-->
