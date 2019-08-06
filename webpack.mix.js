0c1787563ae7b5b39420b0140d4e3b10daa1d954
dangquocdung <dungthinhvn@gmail.com>
2019-08-06 09:20:51 +0700

    Ấasdsa

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.sass('resources/assets/sass/style.scss', 'public/frontend/css')
+mix.sass('resources/assets/sass/app.scss', 'public/frontend/css')
     .version();
 
 mix.disableNotifications();
