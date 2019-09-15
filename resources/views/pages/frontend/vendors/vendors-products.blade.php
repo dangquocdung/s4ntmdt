687e490ee2e8123435f1d28fbd7c8723e03b9264
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-09-15 09:36:49 +0700

    áds

--- resources/views/pages/frontend/vendors/vendors-products.blade.php
+++ resources/views/pages/frontend/vendors/vendors-products.blade.php
@@ -1,5 +1,24 @@
 @section('vendors-products-page-content')
-  
+
+  <!-- Shop Toolbar-->
+  <div class="shop-toolbar padding-bottom-1x mb-2">
+    
+    <div class="column">
+      <div class="shop-view">
+        @if($vendor_products['selected_view'] == 'grid')
+          <a class="grid-view active" href="{{ $vendor_products['action_url_grid_view'] }}" data-toggle="tooltip" data-placement="top" title="{{ trans('frontend.grid_label') }}"><span></span><span></span><span></span></a> 
+        @else  
+          <a class="grid-view" href="{{ $vendor_products['action_url_grid_view'] }}" data-toggle="tooltip" data-placement="top" title="{{ trans('frontend.grid_label') }}"><span></span><span></span><span></span></a>
+        @endif
+
+        @if($vendor_products['selected_view'] == 'list')
+          <a class="list-view active" href="{{ $vendor_products['action_url_list_view'] }}" data-toggle="tooltip" data-placement="top" title="{{ trans('frontend.list_label') }}"><span></span><span></span><span></span></a>
+        @else  
+          <a class="list-view" href="{{ $vendor_products['action_url_list_view'] }}" data-toggle="tooltip" data-placement="top" title="{{ trans('frontend.list_label') }}"><span></span><span></span><span></span></a>
+        @endif
+      </div>
+    </div>
+  </div>
   <div class="products-list">
     <br>  
     @include('includes.frontend.vendor-products')
