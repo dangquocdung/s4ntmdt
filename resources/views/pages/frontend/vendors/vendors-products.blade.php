76022d80c0e658257f4a95ed221ac0d68474ef2f
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-09-15 09:32:37 +0700

    âs

--- resources/views/pages/frontend/vendors/vendors-products.blade.php
+++ resources/views/pages/frontend/vendors/vendors-products.blade.php
@@ -6,16 +6,16 @@
 <div class="shop-toolbar padding-bottom-1x mb-2">
   <div class="column">
     <div class="shop-view">
-      @if($all_products_details['selected_view'] == 'grid')
-        <a class="grid-view active" href="{{ $all_products_details['action_url_grid_view'] }}" data-toggle="tooltip" data-placement="top" title="{{ trans('frontend.grid_label') }}"><span></span><span></span><span></span></a> 
+      @if($vendor_products['selected_view'] == 'grid')
+        <a class="grid-view active" href="{{ $vendor_products['action_url_grid_view'] }}" data-toggle="tooltip" data-placement="top" title="{{ trans('frontend.grid_label') }}"><span></span><span></span><span></span></a> 
       @else  
-        <a class="grid-view" href="{{ $all_products_details['action_url_grid_view'] }}" data-toggle="tooltip" data-placement="top" title="{{ trans('frontend.grid_label') }}"><span></span><span></span><span></span></a>
+        <a class="grid-view" href="{{ $vendor_products['action_url_grid_view'] }}" data-toggle="tooltip" data-placement="top" title="{{ trans('frontend.grid_label') }}"><span></span><span></span><span></span></a>
       @endif
 
-      @if($all_products_details['selected_view'] == 'list')
-        <a class="list-view active" href="{{ $all_products_details['action_url_list_view'] }}" data-toggle="tooltip" data-placement="top" title="{{ trans('frontend.list_label') }}"><span></span><span></span><span></span></a>
+      @if($vendor_products['selected_view'] == 'list')
+        <a class="list-view active" href="{{ $vendor_products['action_url_list_view'] }}" data-toggle="tooltip" data-placement="top" title="{{ trans('frontend.list_label') }}"><span></span><span></span><span></span></a>
       @else  
-        <a class="list-view" href="{{ $all_products_details['action_url_list_view'] }}" data-toggle="tooltip" data-placement="top" title="{{ trans('frontend.list_label') }}"><span></span><span></span><span></span></a>
+        <a class="list-view" href="{{ $vendor_products['action_url_list_view'] }}" data-toggle="tooltip" data-placement="top" title="{{ trans('frontend.list_label') }}"><span></span><span></span><span></span></a>
       @endif
     </div>
   </div>
