462846ee3e43f8bf2173e8caa981d727c1734dda
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-09-17 17:44:55 +0700

    ádsa

--- resources/views/frontend-templates/home/httrade/httrade.blade.php
+++ resources/views/frontend-templates/home/httrade/httrade.blade.php
@@ -45,7 +45,7 @@
           <div class="col-lg-5 col-md-6 padding-bottom-2x text-md-left text-center">
             <div class="from-bottom"><img class="d-inline-block w-150 mb-4" src="img/hero-slider/logo02.png" alt="Puma">
               <div class="h2 text-body mb-2 pt-1">Google Home - Smart Speaker</div>
-              <div class="h2 text-body mb-4 pb-1">starting at <span class="text-medium">2.000.000đ</span></div>
+              <div class="h2 text-body mb-4 pb-1">giá từ <span class="text-medium">2.000.000đ</span></div>
             </div><a class="btn btn-primary scale-up delay-1" href="{{ route('shop-page') }}">Xem ưu đãi&nbsp;<i class="icon-arrow-right"></i></a>
           </div>
           <div class="col-md-6 padding-bottom-2x mb-3"><img class="d-block mx-auto" src="img/hero-slider/02.png" alt="Puma Backpack"></div>
@@ -58,7 +58,7 @@
           <div class="col-lg-5 col-md-6 padding-bottom-2x text-md-left text-center">
             <div class="from-bottom"><img class="d-inline-block w-150 mb-3" src="img/hero-slider/logo01.png" alt="Sony">
               <div class="h2 text-body mb-2 pt-1">Modern Powerful Laptop</div>
-              <div class="h2 text-body mb-4 pb-1">for only <span class="text-medium">10.000.000đ</span></div>
+              <div class="h2 text-body mb-4 pb-1">chỉ từ <span class="text-medium">10.000.000đ</span></div>
             </div><a class="btn btn-primary scale-up delay-1" href="{{ route('shop-page') }}">Mua ngay&nbsp;<i class="icon-arrow-right"></i></a>
           </div>
           <div class="col-md-6 padding-bottom-2x mb-3"><img class="d-block mx-auto" src="img/hero-slider/01.png" alt="Chuck Taylor All Star II"></div>
@@ -71,7 +71,7 @@
           <div class="col-lg-5 col-md-6 padding-bottom-2x text-md-left text-center">
             <div class="from-bottom"><img class="d-inline-block w-150 mb-3" src="img/hero-slider/logo03.png" alt="Motorola">
               <div class="h2 text-body mb-2 pt-1">Beats Studio by Dr.Dre</div>
-              <div class="h2 text-body mb-4 pb-1">for only <span class="text-medium">$349.50</span></div>
+              <div class="h2 text-body mb-4 pb-1">chỉ từ <span class="text-medium">$349.50</span></div>
             </div><a class="btn btn-primary scale-up delay-1" href="{{ route('shop-page') }}">Mua ngay&nbsp;<i class="icon-arrow-right"></i></a>
           </div>
           <div class="col-md-6 padding-bottom-2x mb-3"><img class="d-block mx-auto" src="img/hero-slider/03.png" alt="Moto 360"></div>
