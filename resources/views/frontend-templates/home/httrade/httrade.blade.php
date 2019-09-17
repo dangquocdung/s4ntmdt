8ccdd867653e2b09a418046dfb16d8c970f4f02d
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-09-17 17:38:18 +0700

    ádsa

--- resources/views/frontend-templates/home/httrade/httrade.blade.php
+++ resources/views/frontend-templates/home/httrade/httrade.blade.php
@@ -1,6 +1,6 @@
 <!-- Main Slider-->
 
-<section class="hero-slider" style="background-image: url(img/hero-slider/main-bg.jpg);">
+<!-- <section class="hero-slider" style="background-image: url(img/hero-slider/main-bg.jpg);">
   <div class="owl-carousel large-controls dots-inside" data-owl-carousel='{ "nav": true, "dots": true, "loop": true, "autoplay": true, "autoplayTimeout": 7000 }'>
     
     @foreach($advancedData['recommended_items'] as $key => $recommended_product)
@@ -33,6 +33,51 @@
 
     @endforeach
     
+  </div>
+</section> -->
+
+<!-- Main Slider-->
+<section class="hero-slider" style="background-image: url(img/hero-slider/main-bg.jpg);">
+  <div class="owl-carousel large-controls dots-inside" data-owl-carousel="{ &quot;nav&quot;: true, &quot;dots&quot;: true, &quot;loop&quot;: true, &quot;autoplay&quot;: true, &quot;autoplayTimeout&quot;: 7000 }">
+    <div class="item">
+      <div class="container padding-top-3x">
+        <div class="row justify-content-center align-items-center">
+          <div class="col-lg-5 col-md-6 padding-bottom-2x text-md-left text-center">
+            <div class="from-bottom"><img class="d-inline-block w-150 mb-4" src="img/hero-slider/logo02.png" alt="Puma">
+              <div class="h2 text-body mb-2 pt-1">Google Home - Smart Speaker</div>
+              <div class="h2 text-body mb-4 pb-1">starting at <span class="text-medium">$129.00</span></div>
+            </div><a class="btn btn-primary scale-up delay-1" href="shop-grid-ls.html">View Offers&nbsp;<i class="icon-arrow-right"></i></a>
+          </div>
+          <div class="col-md-6 padding-bottom-2x mb-3"><img class="d-block mx-auto" src="img/hero-slider/02.png" alt="Puma Backpack"></div>
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="container padding-top-3x">
+        <div class="row justify-content-center align-items-center">
+          <div class="col-lg-5 col-md-6 padding-bottom-2x text-md-left text-center">
+            <div class="from-bottom"><img class="d-inline-block w-150 mb-3" src="img/hero-slider/logo01.png" alt="Sony">
+              <div class="h2 text-body mb-2 pt-1">Modern Powerful Laptop</div>
+              <div class="h2 text-body mb-4 pb-1">for only <span class="text-medium">$1,459.99</span></div>
+            </div><a class="btn btn-primary scale-up delay-1" href="{{ route('shop-page') }}">Shop Now&nbsp;<i class="icon-arrow-right"></i></a>
+          </div>
+          <div class="col-md-6 padding-bottom-2x mb-3"><img class="d-block mx-auto" src="img/hero-slider/01.png" alt="Chuck Taylor All Star II"></div>
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="container padding-top-3x">
+        <div class="row justify-content-center align-items-center">
+          <div class="col-lg-5 col-md-6 padding-bottom-2x text-md-left text-center">
+            <div class="from-bottom"><img class="d-inline-block w-150 mb-3" src="img/hero-slider/logo03.png" alt="Motorola">
+              <div class="h2 text-body mb-2 pt-1">Beats Studio by Dr.Dre</div>
+              <div class="h2 text-body mb-4 pb-1">for only <span class="text-medium">$349.50</span></div>
+            </div><a class="btn btn-primary scale-up delay-1" href="{{ route('shop-page') }}">Shop Now&nbsp;<i class="icon-arrow-right"></i></a>
+          </div>
+          <div class="col-md-6 padding-bottom-2x mb-3"><img class="d-block mx-auto" src="img/hero-slider/03.png" alt="Moto 360"></div>
+        </div>
+      </div>
+    </div>
   </div>
 </section>
 <!-- Top Categories/Deals-->
