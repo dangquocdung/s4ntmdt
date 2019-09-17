da44d41f795b74bc5f65d4d5f9e3d95086b2c7fd
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-09-17 08:47:28 +0700

    asdsa

--- resources/views/pages/ajax-pages/cart-html.blade.php
+++ resources/views/pages/ajax-pages/cart-html.blade.php
@@ -182,7 +182,7 @@
             <!-- Product-->
             <div class="product-card">
 
-              @if ( $products['price'] < $products['regular_price'] )
+              @if ( $products['product_price'] < $products['regular_price'] )
                 <div class="product-badge bg-danger">Giảm giá</div>
               @endif
 
@@ -201,7 +201,7 @@
                 <h4 class="product-price">
 
 
-                  @if ( $products['price'] < $products['regular_price'] )
+                  @if ( $products['product_price'] < $products['regular_price'] )
                     <del>
                       {!! price_html( get_product_price_html_by_filter(get_role_based_price_by_product_id($products['id'], $products['regular_price'])), get_frontend_selected_currency()) !!}
                     </del>
