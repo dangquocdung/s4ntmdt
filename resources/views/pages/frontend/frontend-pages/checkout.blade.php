06dd5df680b1317f0c4d82cd17cbd66502cacb0f
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-09-20 13:56:42 +0700

    asdasd

--- resources/views/pages/frontend/frontend-pages/checkout.blade.php
+++ resources/views/pages/frontend/frontend-pages/checkout.blade.php
@@ -153,38 +153,57 @@
             @include('pages.ajax-pages.cart-total-html')
 
             @if ($seen_items <> '')
-            <!-- Featured Products Widget-->
-            <section class="widget widget-featured-products">
-              <h3 class="widget-title">{{ trans('frontend.sp_da_xem') }}</h3>
+              <!-- Featured Products Widget-->
+              <section class="widget widget-featured-products">
+                <h3 class="widget-title">{{ trans('frontend.sp_da_xem') }}</h3>
 
-              @foreach($seen_items as $products)
-              <?php 
-                  $reviews          = get_comments_rating_details($products['id'], 'product');
-                  $reviews_settings = get_reviews_settings_data($products['id']);      
-              ?>
+                @foreach($seen_items as $products)
+                <?php 
+                    $reviews          = get_comments_rating_details($products['id'], 'product');
+                    $reviews_settings = get_reviews_settings_data($products['id']);      
+                ?>
 
-              <!-- Entry-->
-              <div class="entry">
-                <div class="entry-thumb">
-                  <a href="shop-single.html">
-                    @if($products['product_image'])
-                    <img src="{{ get_image_url($products['product_image']) }}" alt="{{ basename($products['product_image']) }}" />
-                    @else
-                    <img src="{{ default_placeholder_img_src() }}" alt="" />
+                <!-- Entry-->
+                <div class="entry">
+                  <div class="entry-thumb">
+                    <a href="{{ route('details-page', $products['post_slug']) }">
+                      @if($products['product_image'])
+                      <img src="{{ get_image_url($products['product_image']) }}" alt="{{ basename($products['product_image']) }}" />
+                      @else
+                      <img src="{{ default_placeholder_img_src() }}" alt="" />
+                      @endif
+                    </a>
+                  </div>
+                  <div class="entry-content">
+                    <h4 class="entry-title">
+                      <a href="{{ route('details-page', $products['post_slug']) }}">{!! $products['post_title'] !!}</a>
+                    </h4>
+                    <span class="entry-meta">
+
+                    @if ( $products['product_price'] < $products['product_regular_price'] )
+                    <del>
+                        {!! price_html( get_product_price_html_by_filter(get_role_based_price_by_product_id($products['id'], $products['regular_price'])), get_frontend_selected_currency()) !!}
+                    </del>
                     @endif
-                  </a>
+                    @if(get_product_type($products['id']) == 'simple_product')
+                    {!! price_html( get_product_price($products['id']), get_frontend_selected_currency() ) !!}
+                    @elseif(get_product_type($products['id']) == 'configurable_product')
+                    {!! get_product_variations_min_to_max_price_html(get_frontend_selected_currency(), $products['id']) !!}
+                    @elseif(get_product_type($products['id']) == 'customizable_product' || get_product_type($products['id']) == 'downloadable_product')
+                    @if(count(get_product_variations($products['id']))>0)
+                        {!! get_product_variations_min_to_max_price_html(get_frontend_selected_currency(), $products['id']) !!}
+                    @else
+                        {!! price_html( get_product_price($products['id']), get_frontend_selected_currency() ) !!}
+                    @endif
+                    @endif
+                      
+                    </span>
+                  </div>
                 </div>
-                <div class="entry-content">
-                  <h4 class="entry-title"><a href="shop-single.html">GoPro Hero4 Silver</a></h4><span class="entry-meta">$287.99</span>
-                </div>
-              </div>
 
-              @endforeach
+                @endforeach
 
-
-        
-              
-            </section>
+              </section>
             @endif
 
             <!-- Promo Banner-->
