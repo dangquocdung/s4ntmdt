9e5d15625c5fd85bfddd47579d8cf2026d10e6b3
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-09-25 21:44:49 +0700

    asdsad

--- resources/views/pages/frontend/frontend-pages/checkout.blade.php
+++ resources/views/pages/frontend/frontend-pages/checkout.blade.php
@@ -354,27 +354,35 @@
 
           <!-- Xác thực người dùng -->
           @if($_settings_data['general_settings']['checkout_options']['enable_login_user'] == true && $is_user_login == false)
+
             <div id="authentication" class="step well">
-              <h2 class="step-title">{!! trans('frontend.authentication_label') !!}</h2><hr>  
-              <div class="user-login-content">
-                <div class="login-information clearfix">
-                  <div class="login-content-sub">
-                    <h3 class="page-subheading">{!! trans('frontend.create_an_account_label') !!}</h3>
-                    <div class="form_content">
-                      <p>{!! trans('frontend.no_user_account_msg') !!}</p>
-                      <a class="btn btn-secondary" href="{{ route('user-registration-page') }}">{!! trans('frontend.create_account_label') !!}</a>
+              <div class="user-login-contentb">
+                <h4>{!! trans('frontend.authentication_label') !!}</h4>
+                <div class="user-login-content">
+                  <div class="login-information clearfix">
+                    <div class="row">
+                      <div class="col-sm-6">
+                        <div class="form_content">
+                          <p>{!! trans('frontend.no_user_account_msg') !!}</p>
+                          <a class="btn btn-secondary" href="{{ route('user-registration-page') }}">{!! trans('frontend.create_account_label') !!}</a>
+                        </div>
+                      </div>
+
+                      <div class="col-sm-6">
+                        <div class="form_content">
+                          <p>{!! trans('frontend.has_user_account_msg') !!}</p>
+                          <a class="btn btn-secondary" href="{{ route('user-login-page') }}">{!! trans('frontend.signin_account_label') !!}</a>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                  <div class="login-content-sub">
-                    <h3 class="page-subheading">{!! trans('frontend.already_registered_label') !!}</h3>
-                    <div class="form_content">
-                      <p>{!! trans('frontend.has_user_account_msg') !!}</p>
-                      <a class="btn btn-secondary" href="{{ route('user-login-page') }}">{!! trans('frontend.signin_account_label') !!}</a>
-                    </div>
-                  </div>
-                </div>  
+
+                              </div>
+                              </div>
+
               </div>
+
             </div>
+
           @endif
 
           <!-- Login user address -->
