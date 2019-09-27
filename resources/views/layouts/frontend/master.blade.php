d1b374e36bf81de4a80e32b168a857bbf83f3122
dangquocdung <dungthinhvn@gmail.com>
2019-09-27 08:29:23 +0700

    ádsa

--- resources/views/layouts/frontend/master.blade.php
+++ resources/views/layouts/frontend/master.blade.php
@@ -114,8 +114,6 @@
     <!-- JavaScript (jQuery) libraries, plugins and custom scripts-->
     <script type="text/javascript" src="{{ URL::asset('/js/vendor.min.js') }}"></script>
     <script type="text/javascript" src="{{ URL::asset('/plugins/iCheck/icheck.min.js') }}"></script>
-    <script type="text/javascript" src="{{ URL::asset('/dropzone/dropzone.js') }}"></script>
-
     <script type="text/javascript" src="{{ URL::asset('/js/common.js') }}"></script>
     <script type="text/javascript" src="{{ mix('/js/app.js') }}"></script>
 
