0b0521068c8bfc27c21ccc4e778354f3afa99d14
dangquocdung <dungthinhvn@gmail.com>
2019-09-30 11:58:19 +0700

    ádsa

--- resources/views/frontend-templates/blog/httrade/httrade.blade.php
+++ resources/views/frontend-templates/blog/httrade/httrade.blade.php
@@ -43,10 +43,12 @@
 
                 @php
 
+                  Carbon\Carbon::setLocale('vi')
+
                   $ngaytao = Carbon\Carbon::parse($row['created_at']);
                     
                 @endphp
-                <li><i class="icon-clock"></i><a href="#">{{ $ngaytao->locale('vi')->format('d F, Y') }}</a></li>
+                <li><i class="icon-clock"></i><a href="#">{{ $ngaytao->format('d F, Y') }}</a></li>
                 <!-- <li><i class="icon-user"></i><a href="#">Paul G.</a></li>
                 <li><i class="icon-tag"></i><a href="#">Video Games</a></li> -->
               </ul>
