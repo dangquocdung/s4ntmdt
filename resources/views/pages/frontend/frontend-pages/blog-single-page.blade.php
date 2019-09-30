364432f6699cad162fd1fb78c33c4b6ce7c3c89a
dangquocdung <dungthinhvn@gmail.com>
2019-09-30 12:33:35 +0700

    asdsa

--- resources/views/pages/frontend/frontend-pages/blog-single-page.blade.php
+++ resources/views/pages/frontend/frontend-pages/blog-single-page.blade.php
@@ -3,7 +3,7 @@
 @if(!empty($blog_details_by_slug['blog_seo_title']))
   @section('title',  $blog_details_by_slug['blog_seo_title'] .' | '. get_site_title())
 @else
-  @section('title',  trans('frontend.blog_details_page_label') .' | '. get_site_title())
+  @section('title',  $blog_details_by_slug['post_title'] .' | '. get_site_title())
 @endif
 
 @section('content')
