ddd4e6da42b9dfc5f9234f2fb20d6d1634428469
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-09-30 23:46:51 +0700

    dfsfs

--- resources/views/pages/frontend/frontend-pages/custom-single-page.blade.php
+++ resources/views/pages/frontend/frontend-pages/custom-single-page.blade.php
@@ -2,9 +2,35 @@
 @section('title', $page_data->post_title .' | '. get_site_title())
 
 @section('content')
-<div id="custom_single_page">
+
+<!-- Page Title-->
+<div class="page-title">
   <div class="container">
-    {!! string_decode($page_data->post_content) !!}
+    <div class="column">
+      <h1>{!! $page_data->post_title !!}</h1>
+    </div>
+    <div class="column">
+      <ul class="breadcrumbs">
+        <li>
+          <a href="{{ route('home-page') }}">{{ trans('frontend.home') }}</a>
+        </li>
+        <li class="separator">&nbsp;</li>
+        <li>{{ trans('frontend.ho_tro') }}</li>
+      </ul>
+    </div>
   </div>
 </div>
+
+<!-- Page Content-->
+<div class="container padding-bottom-3x mb-2">
+  <div class="row justify-content-center">
+    <!-- Content-->
+    <div class="col-xl-9 col-lg-8 order-lg-2">
+      {!! string_decode($page_data->post_content) !!}
+    </div>
+  </div>
+</div>
+
+
+]
 @endsection
