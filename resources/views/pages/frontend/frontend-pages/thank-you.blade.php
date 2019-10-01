c25fea01517ec9ce953af00d6d3b1e3b8612c260
dangquocdung <dungthinhvn@gmail.com>
2019-10-01 12:58:42 +0700

    sadsa

--- resources/views/pages/frontend/frontend-pages/thank-you.blade.php
+++ resources/views/pages/frontend/frontend-pages/thank-you.blade.php
@@ -27,7 +27,7 @@
           <div class="card-body padding-top-2x">
             <h3 class="card-title">{{ trans('frontend.order_received') }}</h3>
             <p class="card-text">{{ trans('frontend.thank_you_msg') }}</p>
-            {{ trans('frontend.order_number') }}:&nbsp; <span class="text-medium">#{!! $order_details_for_thank_you_page['order_id'] !!}</span> - 
+            <p class="card-text">{{ trans('frontend.order_number') }}:&nbsp; <span class="text-medium">#{!! $order_details_for_thank_you_page['order_id'] !!}</span> - 
             {{ trans('frontend.date') }}:&nbsp; <span class="text-medium">{!! $order_details_for_thank_you_page['order_date'] !!}</span> - 
             {{ trans('frontend.total') }}:&nbsp; <span class="text-medium">{!! price_html( $order_details_for_thank_you_page['_final_order_total'], $order_details_for_thank_you_page['_order_currency'] ) !!}</span> - 
             {{ trans('frontend.payment_method') }}:&nbsp; <span class="text-medium">{!! get_payment_method_title($order_details_for_thank_you_page['_payment_method']) !!}</span></p>
@@ -35,18 +35,18 @@
             @if(isset($order_details_for_thank_you_page['_payment_details']['method_instructions']))  
             <div class="row">
                 <div class="col-12">
-                  <p class="card-text payment_ins">*{!! $order_details_for_thank_you_page['_payment_details']['method_instructions'] !!}</p>
+                  <p class="payment_ins">*{!! $order_details_for_thank_you_page['_payment_details']['method_instructions'] !!}</p>
                 </div>
             </div>
             @endif
 
             <div class="padding-top-1x padding-bottom-1x">
-              <a class="btn btn-outline-primary" href="{{ route('shop-page') }}">
+              <a class="btn btn-outline-secondary" href="{{ route('shop-page') }}">
                 <i class="icon-shopping-cart"></i>&nbsp;{{ trans('frontend.tiep_tuc_mua_sam') }}
               </a>
-              {{-- <a class="btn btn-outline-primary" href="#">
-                <i class="icon-map-pin"></i>&nbsp;{{ trans('frontend.theo_doi_don_hang') }}
-              </a> --}}
+              <a class="btn btn-outline-primary" href="#">
+                <i class="icon-map-pin"></i>&nbsp;{{ trans('frontend.chi_tiet_don_hang') }}
+              </a>
             </div>
 
           </div>
