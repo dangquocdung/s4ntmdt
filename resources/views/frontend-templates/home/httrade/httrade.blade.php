93c14734eb475855f9fb842b385d8ab5b9e52206
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-10-01 19:33:44 +0700

    ádasd

--- resources/views/frontend-templates/home/httrade/httrade.blade.php
+++ resources/views/frontend-templates/home/httrade/httrade.blade.php
@@ -354,18 +354,18 @@
         <img class="d-block w-100" src="img/banners/shop-banner-logo.png" alt="Canon">
       </div>
       <div class="pt-5"></div>
-      <div class="countdown countdown-inverse" data-date-time="9/30/2019 00:00:00">
+      <div class="countdown countdown-inverse" data-date-time="10/10/2019 00:00:00">
         <div class="item">
-          <div class="days">00</div><span class="days_ref">Days</span>
+          <div class="days">00</div><span class="days_ref">Ngày</span>
         </div>
         <div class="item">
-          <div class="hours">00</div><span class="hours_ref">Hours</span>
+          <div class="hours">00</div><span class="hours_ref">Giờ</span>
         </div>
         <div class="item">
-          <div class="minutes">00</div><span class="minutes_ref">Mins</span>
+          <div class="minutes">00</div><span class="minutes_ref">Phút</span>
         </div>
         <div class="item">
-          <div class="seconds">00</div><span class="seconds_ref">Secs</span>
+          <div class="seconds">00</div><span class="seconds_ref">Giây</span>
         </div>
       </div>
     </div>
