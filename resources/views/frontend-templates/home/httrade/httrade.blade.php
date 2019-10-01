7859170e8069aafba32231ea1c9ca5c2ab13b325
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-10-02 01:15:54 +0700

    sadsad

--- resources/views/frontend-templates/home/httrade/httrade.blade.php
+++ resources/views/frontend-templates/home/httrade/httrade.blade.php
@@ -258,6 +258,82 @@
 
 </section>
 
+<!-- Latest Products-->
+<section class="container padding-bottom-2x mb-2">
+  <h2 class="h3 pb-3 text-center">{{ trans('frontend.latest_products') }}</h2>
+  <div class="row">
+
+  @foreach($advancedData['latest_items'] as $key => $latest_product)
+
+    <div class="col-lg-3 col-md-4 col-sm-6">
+      <div class="product-card mb-30">
+
+        @if ( $latest_product->price < $latest_product->regular_price )
+          @php
+
+          $tiengiam =  $latest_product->regular_price - $latest_product->price;
+
+          $phantram = round(($tiengiam/$latest_product->regular_price)*100);
+            
+        @endphp
+        <div class="product-badge bg-danger">Giảm giá {{ $phantram }}%</div>
+
+        @endif
+        <a class="product-thumb" href="{{ route('details-page', $latest_product->slug) }}">
+          @if(!empty($latest_product->image_url))
+          <img class="products-page-product-img" src="{{ get_image_url( $latest_product->image_url ) }}" alt="{{ basename( get_image_url( $latest_product->image_url ) ) }}" />
+          @else
+          <img class="products-page-product-img" src="{{ default_placeholder_img_src() }}" alt="" />
+          @endif
+        </a>
+        <div class="product-card-body">
+          
+          <h3 class="product-title"><a href="{{ route('details-page', $latest_product->slug) }}">{!! $latest_product->title !!}</a></h3>
+          <h4 class="product-price">
+            @if ( $latest_product->price < $latest_product->regular_price )
+              <del>
+                {!! price_html( get_product_price_html_by_filter(get_role_based_price_by_product_id($latest_product->id, $latest_product->regular_price)), get_frontend_selected_currency()) !!}
+              </del>
+            @endif
+
+            @if( $latest_product->type == 'simple_product' )
+              {!! price_html( get_product_price_html_by_filter(get_role_based_price_by_product_id($latest_product->id, $latest_product->price)), get_frontend_selected_currency()) !!}
+            @elseif( $latest_product->type == 'configurable_product' )
+              {!! get_product_variations_min_to_max_price_html(get_frontend_selected_currency(), $latest_product->id) !!}
+            @elseif( $latest_product->type == 'customizable_product' || $latest_product->type == 'downloadable_product')
+              @if(count(get_product_variations($latest_product->id))>0)
+              {!! get_product_variations_min_to_max_price_html(get_frontend_selected_currency(), $latest_product->id) !!}
+              @else
+              {!! price_html( get_product_price_html_by_filter(get_role_based_price_by_product_id($latest_product->id, $latest_product->price)), get_frontend_selected_currency()) !!}
+              @endif
+            @endif
+          </h4>
+        </div>
+        <div class="product-button-group">
+
+          <a class="product-button btn-wishlist product-wishlist" data-id="{{ $latest_product->id }}" data-toggle="tooltip" title="{{ trans('frontend.add_to_wishlist_label') }}" data-original-title="{{ trans('frontend.add_to_wishlist_label') }}">
+            <i class="icon-heart"></i><span>{{ trans('frontend.add_to_wishlist_label') }}</span>
+          </a>
+          <a class="product-button btn-compare product-compare" data-id="{{ $latest_product->id }}" data-toggle="tooltip" title="" data-original-title="{{ trans('frontend.add_to_compare_list_label') }}">
+            <i class="icon-repeat"></i><span>{{ trans('frontend.add_to_compare_list_label') }}</span>
+          </a>
+          <a class="product-button add-to-cart-bg" data-toast data-toast-type="success" data-toast-position="topRight" data-toast-icon="icon-check-circle" data-toast-title="Sản phẩm" data-toast-message="{{ trans('frontend.successfuly_added_to_cart') }}" data-id="{{ $latest_product->id }}" data-toggle="tooltip" data-placement="top"title="" data-original-title="{{ trans('frontend.add_to_cart_label') }}">
+            <i class="icon-shopping-cart"></i><span>{{ trans('frontend.add_to_cart_label') }}</span>
+          </a>
+
+       </div>
+      </div>
+    </div>  
+  @endforeach
+  </div>
+
+  <div class="text-center">
+    <a class="btn btn-outline-secondary" href="{{ route('shop-page') }}">{{ trans('frontend.view_all_products') }}</a>
+  </div>
+
+</section>
+
+
 
 <!-- Today sale Products-->
 
