2a4bd7a769c376959d932d0ff2a7f7857c383795
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-10-02 11:47:04 +0700

    ádsa

--- resources/views/pages/frontend/frontend-pages/coming.blade.php
+++ resources/views/pages/frontend/frontend-pages/coming.blade.php
@@ -2,5 +2,5 @@
 @section('title', trans('frontend.shopist_shop_title') .' | '. get_site_title() )
 
 @section('content')
-  @include( 'frontend-templates.coming.htttrade.httrade )
+  @include( 'frontend-templates.coming.httrade.httrade )
 @endsection  
