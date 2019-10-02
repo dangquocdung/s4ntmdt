7b203f3c5490322466b30b4b6bbe0241819d85be
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-10-02 11:46:13 +0700

    ádasasad

--- resources/views/pages/frontend/frontend-pages/coming.blade.php
+++ resources/views/pages/frontend/frontend-pages/coming.blade.php
@@ -1,10 +1,6 @@
 @extends('layouts.frontend.master')
 @section('title', trans('frontend.shopist_shop_title') .' | '. get_site_title() )
-@section('breadcrumb',trans('frontend.products'))
-
 
 @section('content')
-<div id="shop_page">
   @include( 'frontend-templates.coming.htttrade.httrade )
-</div>	
 @endsection  
--- resources/views/pages/frontend/frontend-pages/products.blade.php
+++ resources/views/pages/frontend/frontend-pages/products.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.frontend.master')
 @section('title', trans('frontend.shopist_shop_title') .' | '. get_site_title() )
-@section('breadcrumb',trans('frontend.products'))
-
 
 @section('content')
 <div id="shop_page">
