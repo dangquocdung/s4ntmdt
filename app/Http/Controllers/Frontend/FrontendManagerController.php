8cefc212a4a51c59a524cbef765438fb51dfe2e9
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-10-02 11:49:46 +0700

    ádsa

--- app/Http/Controllers/Frontend/FrontendManagerController.php
+++ app/Http/Controllers/Frontend/FrontendManagerController.php
@@ -88,17 +88,7 @@ class FrontendManagerController extends Controller
 
   public function comingSoon(){
 
-    $data = array();
-    
-    $data = $this->classCommonFunction->get_dynamic_frontend_content_data(); 
-
-    $data['advancedData']        =   $this->product->getAdvancedProducts();
-
-    $data['brands_data']         =   $this->product->getTermData( 'product_brands', false, null, 1 );
-    
-    $data['testimonials_data']   =   get_all_testimonial_data();
-
-    return view('pages.frontend.frontend-pages.coming', $data);
+    return view('pages.frontend.frontend-pages.coming');
 
   }
 
