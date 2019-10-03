010c616ef6a2a2266c392a4794e8b504dce8d824
dangquocdung <dungthinhvn@gmail.com>
2019-10-03 15:41:58 +0700

    ádsa

--- resources/views/pages/frontend/user-account/order-details.blade.php
+++ resources/views/pages/frontend/user-account/order-details.blade.php
@@ -4,22 +4,21 @@
   <div class="w-100 text-center py-1 px-2"><span class='text-medium'>Status:</span> Processing Order</div>
   <div class="w-100 text-center py-1 px-2"><span class='text-medium'>Expected Date:</span> Feb 26, 2018</div>
 </div>
-<div class="card-body">
-  <div class="steps flex-sm-nowrap padding-top-1x padding-bottom-1x">
-    <div class="step active"><i class="icon-shopping-bag"></i>
-      <h4 class="step-title">Confirmed Order</h4>
-    </div>
-    <div class="step active"><i class="icon-settings"></i>
-      <h4 class="step-title">Processing Order</h4>
-    </div>
-    <div class="step"><i class="icon-award"></i>
-      <h4 class="step-title">Quality Check</h4>
-    </div>
-    <div class="step"><i class="icon-truck"></i>
-      <h4 class="step-title">Product Dispatched</h4>
-    </div>
-    <div class="step"><i class="icon-home"></i>
-      <h4 class="step-title">Product Delivered</h4>
-    </div>
+
+<div class="steps flex-sm-nowrap padding-top-1x padding-bottom-1x">
+  <div class="step active"><i class="icon-shopping-bag"></i>
+    <h4 class="step-title">Confirmed Order</h4>
+  </div>
+  <div class="step active"><i class="icon-settings"></i>
+    <h4 class="step-title">Processing Order</h4>
+  </div>
+  <div class="step"><i class="icon-award"></i>
+    <h4 class="step-title">Quality Check</h4>
+  </div>
+  <div class="step"><i class="icon-truck"></i>
+    <h4 class="step-title">Product Dispatched</h4>
+  </div>
+  <div class="step"><i class="icon-home"></i>
+    <h4 class="step-title">Product Delivered</h4>
   </div>
 </div>
