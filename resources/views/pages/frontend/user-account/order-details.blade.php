e2b7a6dad6824dd6810a69e4b477fb0d74900339
dangquocdung <dungthinhvn@gmail.com>
2019-10-03 15:41:08 +0700

    ádsad

--- resources/views/pages/frontend/user-account/order-details.blade.php
+++ resources/views/pages/frontend/user-account/order-details.blade.php
@@ -1,35 +1,25 @@
 <!-- Page Content-->
-  <div class="card mb-3">
-    <div class="p-4 text-center text-white bg-dark rounded-top"><span class="text-uppercase">Tracking Order No - </span><span class="text-medium">34VB5540K83</span></div>
-    <div class="d-flex flex-wrap flex-sm-nowrap justify-content-between py-3 px-2 bg-secondary">
-      <div class="w-100 text-center py-1 px-2"><span class='text-medium'>Shipped Via:</span> UPS Ground</div>
-      <div class="w-100 text-center py-1 px-2"><span class='text-medium'>Status:</span> Processing Order</div>
-      <div class="w-100 text-center py-1 px-2"><span class='text-medium'>Expected Date:</span> Feb 26, 2018</div>
+<div class="d-flex flex-wrap flex-sm-nowrap justify-content-between py-3 px-2 bg-secondary">
+  <div class="w-100 text-center py-1 px-2"><span class='text-medium'>Shipped Via:</span> UPS Ground</div>
+  <div class="w-100 text-center py-1 px-2"><span class='text-medium'>Status:</span> Processing Order</div>
+  <div class="w-100 text-center py-1 px-2"><span class='text-medium'>Expected Date:</span> Feb 26, 2018</div>
+</div>
+<div class="card-body">
+  <div class="steps flex-sm-nowrap padding-top-1x padding-bottom-1x">
+    <div class="step active"><i class="icon-shopping-bag"></i>
+      <h4 class="step-title">Confirmed Order</h4>
     </div>
-    <div class="card-body">
-      <div class="steps flex-sm-nowrap padding-top-1x padding-bottom-1x">
-        <div class="step active"><i class="icon-shopping-bag"></i>
-          <h4 class="step-title">Confirmed Order</h4>
-        </div>
-        <div class="step active"><i class="icon-settings"></i>
-          <h4 class="step-title">Processing Order</h4>
-        </div>
-        <div class="step"><i class="icon-award"></i>
-          <h4 class="step-title">Quality Check</h4>
-        </div>
-        <div class="step"><i class="icon-truck"></i>
-          <h4 class="step-title">Product Dispatched</h4>
-        </div>
-        <div class="step"><i class="icon-home"></i>
-          <h4 class="step-title">Product Delivered</h4>
-        </div>
-      </div>
+    <div class="step active"><i class="icon-settings"></i>
+      <h4 class="step-title">Processing Order</h4>
+    </div>
+    <div class="step"><i class="icon-award"></i>
+      <h4 class="step-title">Quality Check</h4>
+    </div>
+    <div class="step"><i class="icon-truck"></i>
+      <h4 class="step-title">Product Dispatched</h4>
+    </div>
+    <div class="step"><i class="icon-home"></i>
+      <h4 class="step-title">Product Delivered</h4>
     </div>
   </div>
-  <div class="d-flex flex-wrap flex-md-nowrap justify-content-center justify-content-sm-between align-items-center">
-    <div class="custom-control custom-checkbox mr-3">
-      <input class="custom-control-input" type="checkbox" id="notify_me" checked>
-      <label class="custom-control-label" for="notify_me">Notify me when order is delivered</label>
-    </div>
-    <div class="text-left text-sm-right"><a class="btn btn-primary btn-sm" href="orderDetails" data-toggle="modal" data-target="#orderDetails">View Order Details</a></div>
-  </div>
+</div>
