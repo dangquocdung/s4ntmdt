5c7ce00c9c90ed0364e611bac180fadfa92e1b45
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-10-19 15:00:08 +0700

    asdasd

--- resources/views/includes/frontend/head.blade.php
+++ resources/views/includes/frontend/head.blade.php
@@ -68,4 +68,4 @@
 
     <!-- Modernizr-->
     <script src="{{ URL::asset('/js/modernizr.min.js') }}"></script>
-    <script src='https://www.google.com/recaptcha/api.js'></script>
+    <!-- <script src='https://www.google.com/recaptcha/api.js'></script> -->
