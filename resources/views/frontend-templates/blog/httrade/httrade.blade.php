88a227c3600e954559bef4026058bf9fdd1cf5b9
dangquocdung <dungthinhvn@gmail.com>
2019-10-21 15:21:26 +0700

    ádas

--- resources/views/frontend-templates/blog/httrade/httrade.blade.php
+++ resources/views/frontend-templates/blog/httrade/httrade.blade.php
@@ -51,10 +51,6 @@
 
               </ul>
               <h3 class="post-title"><a href="{{ route('blog-single-page', $row['post_slug']) }}">{!! $row['post_title'] !!}</a></h3>
-              <p>
-                {{-- {!! strip_tags(get_limit_string(string_decode($row['post_content']), $row['allow_max_number_characters_at_frontend'])) !!}
-                <a href="{{ route('blog-single-page', $row['post_slug']) }}" class="text-medium">{!! trans('frontend.read_more_label') !!}</a> --}}
-              </p>
             </div>
           </div>
         </div>
