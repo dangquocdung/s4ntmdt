aee9d7bc57b90ee9d38000ccca0115afecf86761
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-10-24 18:13:09 +0700

    asdsa

--- resources/views/frontend-templates/blog/httrade/httrade.blade.php
+++ resources/views/frontend-templates/blog/httrade/httrade.blade.php
@@ -29,54 +29,56 @@
 
     <!-- Blog Posts-->
     <div class="col-lg-9">
-      <div class="container isotope-grid cols-3 mb-4">
-        <div class="gutter-sizer"></div>
-        <div class="grid-sizer"></div>
+      <div class="col-12">
+        <div class="isotope-grid cols-3 mb-4">
+          <div class="gutter-sizer"></div>
+          <div class="grid-sizer"></div>
 
-        @if(count($blogs_all_data) > 0)
+          @if(count($blogs_all_data) > 0)
 
-        @foreach($blogs_all_data as $row)
+          @foreach($blogs_all_data as $row)
 
-        <!-- Post-->
-        
-        <div class="grid-item">
-          <div class="blog-post">
-            <a class="post-thumb" href="{{ route('blog-single-page', $row['post_slug']) }}">
+          <!-- Post-->
+          
+          <div class="grid-item">
+            <div class="blog-post">
+              <a class="post-thumb" href="{{ route('blog-single-page', $row['post_slug']) }}">
 
-            @if(!empty($row['featured_image']))
-                <img src="{{ get_image_url($row['featured_image']) }}" alt="{{ basename($row['featured_image']) }}">
-              @else
-                <img src="{{ default_placeholder_img_src() }}" alt="Blog Post">
-              @endif
-
-            </a>
-            <div class="post-body">
-              <ul class="post-meta">
-                <li><i class="icon-clock"></i><a href="#">{{ Carbon\Carbon::parse($row['created_at'])->format('d-m-Y H:i') }}</a></li>
-                <li><i class="icon-user"></i><a href="#">{{ get_user_name_by_user_id($row['post_author_id']) }}</a></li>
-                @if (!empty($row['post_file']))
-                  <li><i class="icon-file"></i><a href="{{ URL::asset($row['post_file']) }}">Văn bản</a></li>
+              @if(!empty($row['featured_image']))
+                  <img src="{{ get_image_url($row['featured_image']) }}" alt="{{ basename($row['featured_image']) }}">
+                @else
+                  <img src="{{ default_placeholder_img_src() }}" alt="Blog Post">
                 @endif
 
-              </ul>
-              <h3 class="post-title">
-                <a href="{{ route('blog-single-page', $row['post_slug']) }}">{!! $row['post_title'] !!}</a>
-              </h3>
-              <p>
-                {{-- {!! get_limit_string(string_decode($row['post_content']), $row['allow_max_number_characters_at_frontend']) !!} --}}
-                {{-- <a href="{{ route('blog-single-page', $row['post_slug']) }}" class="text-medium">{!! trans('frontend.read_more_label') !!}</a> --}}
-              </p>
+              </a>
+              <div class="post-body">
+                <ul class="post-meta">
+                  <li><i class="icon-clock"></i><a href="#">{{ Carbon\Carbon::parse($row['created_at'])->format('d-m-Y H:i') }}</a></li>
+                  <li><i class="icon-user"></i><a href="#">{{ get_user_name_by_user_id($row['post_author_id']) }}</a></li>
+                  @if (!empty($row['post_file']))
+                    <li><i class="icon-file"></i><a href="{{ URL::asset($row['post_file']) }}">Văn bản</a></li>
+                  @endif
+
+                </ul>
+                <h3 class="post-title">
+                  <a href="{{ route('blog-single-page', $row['post_slug']) }}">{!! $row['post_title'] !!}</a>
+                </h3>
+                <p>
+                  {{-- {!! get_limit_string(string_decode($row['post_content']), $row['allow_max_number_characters_at_frontend']) !!} --}}
+                  {{-- <a href="{{ route('blog-single-page', $row['post_slug']) }}" class="text-medium">{!! trans('frontend.read_more_label') !!}</a> --}}
+                </p>
+              </div>
             </div>
           </div>
+
+          @endforeach
+
+          @else
+            <p>{!! trans('frontend.no_blogs_data_label') !!}</p>
+
+          @endif
+
         </div>
-
-        @endforeach
-
-        @else
-          <p>{!! trans('frontend.no_blogs_data_label') !!}</p>
-
-        @endif
-
       </div>
      
     </div>
