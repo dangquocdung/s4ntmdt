784d6783c44fc6b347ea45624298bf1a3f988292
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-10-28 12:17:15 +0700

    ádsad

--- resources/views/layouts/frontend/master.blade.php
+++ resources/views/layouts/frontend/master.blade.php
@@ -7,30 +7,34 @@
   <!-- Body-->
   <body>
 
-    <!-- Load Facebook SDK for JavaScript -->
-    <div id="fb-root"></div>
-      <script>
-        window.fbAsyncInit = function() {
-          FB.init({
-            xfbml            : true,
-            version          : 'v4.0'
-          });
-        };
+    @if( Request::is('/') )
+      <!-- Load Facebook SDK for JavaScript -->
+      <div id="fb-root"></div>
+        <script>
+          window.fbAsyncInit = function() {
+            FB.init({
+              xfbml            : true,
+              version          : 'v4.0'
+            });
+          };
 
-        (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));</script>
+          (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+      </script>
 
       <!-- Your customer chat code -->
       <div class="fb-customerchat"
         attribution=setup_tool
         page_id="123433239059181"
-  theme_color="#0084ff">
+        theme_color="#0084ff">
       </div>
+
+    @endif
 
     @if(get_appearance_settings()['general']['custom_css'] == true)
       @include('includes.frontend.content-custom-css')
