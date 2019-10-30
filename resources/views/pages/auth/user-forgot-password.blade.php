4173dd226fbef1e4ce0448f86f9016650155950b
dangquocdung <dungthinhvn@gmail.com>
2019-10-30 18:46:13 +0700

    asdas

--- resources/views/pages/auth/user-forgot-password.blade.php
+++ resources/views/pages/auth/user-forgot-password.blade.php
@@ -34,34 +34,28 @@
 
       <form class="card margin-top-1x" method="post" action="" enctype="multipart/form-data">
         <input type="hidden" name="_token" id="_token" value="{{ csrf_token() }}">
-
       
         <div class="card-body">
           <div class="form-group input-group">
             <input type="email" placeholder="{{ ucfirst( trans('frontend.email') ) }}" class="form-control" id="user_forgot_pass_email_id" name="user_forgot_pass_email_id">
-            <span class="fa fa-envelope form-control-feedback"></span>
+            <span class="input-group-addon"><i class="icon-mail"></i></span>
+
 
           </div>
           <div class="form-group input-group">
 
             <input type="password" placeholder="{{ ucfirst(trans('frontend.enter_new_password')) }}" class="form-control" id="user_forgot_new_password" name="user_forgot_new_password">
-            <span class="fa fa-lock form-control-feedback"></span>
+            <span class="input-group-addon"><i class="icon-lock"></i></span>
 
           </div>
 
           <div class="form-group input-group">
             <input type="text" placeholder="{{ ucfirst(trans('frontend.secret_key')) }}" class="form-control" id="user_forgot_secret_key" name="user_forgot_secret_key">
-            <span class="fa fa-lock form-control-feedback"></span>
+            <span class="input-group-addon"><i class="icon-lock"></i></span>
           </div>
-
-          <div class="form-group">
-            <div class="captcha-style">{!! NoCaptcha::display() !!}</div>
-          </div>
-
-          <a class="navi-link" href="{{ route('user-registration-page') }}">{{ trans('frontend.frontend_user_registration_title') }}</a>
 
           <div class="text-center text-sm-right">
-            <button class="btn btn-primary margin-bottom-none" type="submit" name="user_forgot_pass_submit" id="user_forgot_pass_submit" tabindex="4">{{ trans('frontend.freset_my_password') }}</button>
+            <button class="btn btn-primary margin-bottom-none" type="submit" name="user_forgot_pass_submit" id="user_forgot_pass_submit" tabindex="4">{{ trans('frontend.reset_my_password') }}</button>
           </div>
         </div>
       </form>
