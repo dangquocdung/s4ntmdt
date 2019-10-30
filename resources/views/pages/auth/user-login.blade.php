cbf715a589d4a7b9c15432cfe3eb931c0af52d29
dangquocdung <dungthinhvn@gmail.com>
2019-10-30 18:31:05 +0700

    ádsa

--- resources/views/pages/auth/user-login.blade.php
+++ resources/views/pages/auth/user-login.blade.php
@@ -22,61 +22,57 @@
 </div>
 
 <!-- Page Content-->
-<div class="container padding-bottom-3x mb-2" >
+<div class="container padding-bottom-3x mb-2" id="login-page">
+
+    @include('pages-message.notify-msg-error')
+    @include('pages-message.form-submit')
+
     <div class="row">
-      <div class="col-md-6 col-sm-12" id="login-page">
-        
-        @include('pages-message.notify-msg-error')
-        @include('pages-message.form-submit')
-
-        <form class="login-box" method="post" action="" enctype="multipart/form-data">
-          <input type="hidden" name="_token" id="_token" value="{{ csrf_token() }}">
-
-          <div class="form-group input-group">
-            <input name="login_username" id="login_username" tabindex="1" class="form-control" placeholder="{{ trans('frontend.frontend_username_placeholder') }}" value="{{ $frontend_login_data['user'] }}" type="text">
-            <span class="input-group-addon"><i class="icon-mail"></i></span>
-          </div>
-          <div class="form-group input-group">
-            <input name="login_password" id="login_password" tabindex="2" class="form-control" placeholder="{{ trans('frontend.password') }}" type="password" value="{{ $frontend_login_data['pass'] }}">
-            <span class="input-group-addon"><i class="icon-lock"></i></span>
-          </div>
-
-          <div class="d-flex flex-wrap justify-content-between padding-bottom-1x">
-            <div class="custom-control custom-checkbox">
-              @if (Cookie::has('frontend_remember_me_data'))
-                <input class="custom-control-input" type="checkbox" id="remember_me" checked>
-                <label class="custom-control-label" for="remember_me">{{ trans('frontend.remember_me') }}</label>
-              @else
-                <input class="custom-control-input" type="checkbox" id="remember_me">
-                <label class="custom-control-label" for="remember_me">{{ trans('frontend.remember_me') }}</label>
-              @endif
+      <div class="col-md-6">
+        <form class="card" method="post" action="" enctype="multipart/form-data">
+          <div class="card-body">
+            <div class="row margin-bottom-1x">
+              <div class="col-xl-4 col-md-6 col-sm-4"><a class="btn btn-sm btn-block facebook-btn" href="#"><i class="socicon-facebook"></i>&nbsp;Facebook login</a></div>
+              <div class="col-xl-4 col-md-6 col-sm-4"><a class="btn btn-sm btn-block twitter-btn" href="#"><i class="socicon-twitter"></i>&nbsp;Twitter login</a></div>
+              <div class="col-xl-4 col-md-6 col-sm-4"><a class="btn btn-sm btn-block google-btn" href="#"><i class="socicon-googleplus"></i>&nbsp;Google+ login</a></div>
             </div>
-            <a class="navi-link" href="{{ route('user-registration-page') }}">{{ trans('frontend.frontend_user_registration_title') }}</a>
-
-            <a class="navi-link" href="{{ route('user-forgot-password-page') }}">{{ trans('frontend.forgot_password') }}</a>&nbsp;&nbsp;&nbsp;&nbsp;
-            
-          </div>
-
-          @if($frontend_login_data['is_enable_recaptcha'] == true)
-            <div class="form-group">
-              <div class="captcha-style">{!! NoCaptcha::display() !!}</div>
+            <h4 class="margin-bottom-1x">Hoặc điền thông tin vào biểu mẫu</h4>
+            <div class="form-group input-group">
+              <input name="login_username" id="login_username" tabindex="1" class="form-control" placeholder="{{ trans('frontend.frontend_username_placeholder') }}" value="{{ $frontend_login_data['user'] }}" type="text">
+              <span class="input-group-addon"><i class="icon-mail"></i></span>
             </div>
-          @endif
-          <div class="text-center text-sm-right">
-            <button class="btn btn-primary margin-bottom-none" type="submit" name="login_submit" id="login_submit" tabindex="4">{{ trans('frontend.frontend_log_in') }}</button>
+            <div class="form-group input-group">
+              <input name="login_password" id="login_password" tabindex="2" class="form-control" placeholder="{{ trans('frontend.password') }}" type="password" value="{{ $frontend_login_data['pass'] }}">
+              <span class="input-group-addon"><i class="icon-lock"></i></span>
+            </div>
+            <div class="d-flex flex-wrap justify-content-between padding-bottom-1x">
+              <div class="custom-control custom-checkbox">
+                @if (Cookie::has('frontend_remember_me_data'))
+                  <input class="custom-control-input" type="checkbox" id="remember_me" checked>
+                  <label class="custom-control-label" for="remember_me">{{ trans('frontend.remember_me') }}</label>
+                @else
+                  <input class="custom-control-input" type="checkbox" id="remember_me">
+                  <label class="custom-control-label" for="remember_me">{{ trans('frontend.remember_me') }}</label>
+                @endif
+
+              </div>
+              <a class="navi-link" href="{{ route('user-forgot-password-page') }}">{{ trans('frontend.forgot_password') }}</a>
+            </div>
+
+            @if($frontend_login_data['is_enable_recaptcha'] == true)
+              <div class="form-group">
+                <div class="captcha-style">{!! NoCaptcha::display() !!}</div>
+              </div>
+            @endif
+
+            <div class="text-center text-sm-right">
+              <button class="btn btn-primary margin-bottom-none" type="submit" name="login_submit" id="login_submit" tabindex="4">{{ trans('frontend.frontend_log_in') }}</button>
+            </div>
           </div>
         </form>
-
-        <div class="social-login padding-top-1x">
-          <h6 class="margin-bottom-1x">Hoặc dùng tài khoản mạng xã hội:</h6>
-          <div class="row margin-bottom-1x">
-            <div class="col-4"><a class="btn btn-sm btn-block facebook-btn" href="javascript:void(0)"><i class="socicon-facebook"></i>&nbsp;Facebook</a></div>
-            <div class="col-4"><a class="btn btn-sm btn-block google-btn" href="javascript:void(0)"><i class="socicon-google"></i>&nbsp;Google+</a></div>
-            <div class="col-4"><a class="btn btn-sm btn-block twitter-btn" href="javascript:void(0)"><i class="socicon-twitter"></i>&nbsp;Zalo</a></div>
-          </div>
-        </div>
-
       </div>
+
+     
       
     </div>
   </div>
