5dbdf41f52ba5f8b228abe8ebcc44ec4d606ae69
dangquocdung <dungthinhvn@gmail.com>
2019-10-30 18:43:34 +0700

    asdas

--- resources/views/pages/auth/user-forgot-password.blade.php
+++ resources/views/pages/auth/user-forgot-password.blade.php
@@ -1,57 +1,78 @@
 @extends('layouts.frontend.master')
 @section('title', trans('frontend.frontend_user_forgot_password') .' - '. get_site_title())
+
 @section('content')
-<div class="container"><br>  
-  <div class="row justify-content-center">
-    <div class="col-md-6 text-center">
-      <div class="panel panel-login">
-        <div class="panel-heading">
-          <div class="row justify-content-center">
-            <div class="col-xs-12 text-center">
-              <h3>{{ trans('frontend.forgot_password') }}</h3>
-              <p>{{ trans('frontend.reset_password_msg') }}</p>
-            </div>
-          </div>
-          <hr>
-        </div>
-        <div class="panel-body">
-          <div class="row">
-            <div class="col-lg-12">
-              @include('pages-message.notify-msg-error')
-              @include('pages-message.form-submit')
-              
-              <form method="post" action="" enctype="multipart/form-data">
-                <input type="hidden" name="_token" id="_token" value="{{ csrf_token() }}">
-                
-                <div class="form-group has-feedback">
-                  <input type="email" placeholder="{{ ucfirst( trans('frontend.email') ) }}" class="form-control" id="user_forgot_pass_email_id"name="user_forgot_pass_email_id">
-                  <span class="fa fa-envelope form-control-feedback"></span>
-                </div>
 
-                <div class="form-group has-feedback">
-                  <input type="password" placeholder="{{ ucfirst(trans('frontend.enter_new_password')) }}" class="form-control" id="user_forgot_new_password" name="user_forgot_new_password">
-                  <span class="fa fa-lock form-control-feedback"></span>
-                </div>
-
-                <div class="form-group has-feedback">
-                  <input type="text" placeholder="{{ ucfirst(trans('frontend.secret_key')) }}" class="form-control" id="user_forgot_secret_key" name="user_forgot_secret_key">
-                  <span class="fa fa-lock form-control-feedback"></span>
-                </div>
-
-                <div class="form-group">
-                  <div class="row justify-content-center">
-                    <div class="col-sm-6 text-center">
-                      <input name="user_forgot_pass_submit" id="user_forgot_pass_submit" class="form-control btn btn-secondary" value="{{ trans('frontend.reset_my_password') }}" type="submit">
-                    </div>
-                  </div>
-                </div>
-                
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
+<!-- Page Title-->
+<div class="page-title">
+  <div class="container">
+    <div class="column">
+      <h1>{{ trans('frontend.forgot_password') }}</h1>
+    </div>
+    <div class="column">
+      <ul class="breadcrumbs">
+        <li>
+          <a href="{{ route('home-page') }}">{{ trans('frontend.home') }}</a>
+        </li>
+        <li class="separator">&nbsp;</li>
+        <li>{{ trans('frontend.forgot_password') }}</li>
+      </ul>
     </div>
   </div>
-</div>  
+</div>
+
+<!-- Page Content-->
+<!-- Page Content-->
+<div class="container padding-bottom-3x mb-2" id="login-page">
+
+
+  <div class="row">
+    <div class="col-md-6">
+
+      @include('pages-message.notify-msg-error')
+      @include('pages-message.form-submit')
+
+      <form class="card margin-top-1x" method="post" action="" enctype="multipart/form-data">
+        <input type="hidden" name="_token" id="_token" value="{{ csrf_token() }}">
+
+      
+        <div class="card-body">
+          <div class="form-group input-group">
+            <input type="email" placeholder="{{ ucfirst( trans('frontend.email') ) }}" class="form-control" id="user_forgot_pass_email_id" name="user_forgot_pass_email_id">
+            <span class="fa fa-envelope form-control-feedback"></span>
+
+          </div>
+          <div class="form-group input-group">
+
+            <input type="password" placeholder="{{ ucfirst(trans('frontend.enter_new_password')) }}" class="form-control" id="user_forgot_new_password" name="user_forgot_new_password">
+            <span class="fa fa-lock form-control-feedback"></span>
+
+          </div>
+
+          <div class="form-group input-group">
+            <input type="text" placeholder="{{ ucfirst(trans('frontend.secret_key')) }}" class="form-control" id="user_forgot_secret_key" name="user_forgot_secret_key">
+            <span class="fa fa-lock form-control-feedback"></span>
+          </div>
+
+
+          @if($frontend_login_data['is_enable_recaptcha'] == true)
+            <div class="form-group">
+              <div class="captcha-style">{!! NoCaptcha::display() !!}</div>
+            </div>
+          @endif
+
+          <a class="navi-link" href="{{ route('user-registration-page') }}">{{ trans('frontend.frontend_user_registration_title') }}</a>
+
+          <div class="text-center text-sm-right">
+            <button class="btn btn-primary margin-bottom-none" type="submit" name="user_forgot_pass_submit" id="user_forgot_pass_submit" tabindex="4">{{ trans('frontend.freset_my_password') }}</button>
+          </div>
+        </div>
+      </form>
+    </div>
+
+    
+    
+  </div>
+</div>
+
 @endsection  
--- resources/views/pages/auth/user-login.blade.php
+++ resources/views/pages/auth/user-login.blade.php
@@ -25,64 +25,64 @@
 <div class="container padding-bottom-3x mb-2" id="login-page">
 
 
-    <div class="row">
-      <div class="col-md-6">
+  <div class="row">
+    <div class="col-md-6">
 
-        @include('pages-message.notify-msg-error')
-        @include('pages-message.form-submit')
+      @include('pages-message.notify-msg-error')
+      @include('pages-message.form-submit')
 
-        <form class="card margin-top-1x" method="post" action="" enctype="multipart/form-data">
-          <input type="hidden" name="_token" id="_token" value="{{ csrf_token() }}">
+      <form class="card margin-top-1x" method="post" action="" enctype="multipart/form-data">
+        <input type="hidden" name="_token" id="_token" value="{{ csrf_token() }}">
 
-        
-          <div class="card-body">
-            <div class="row margin-bottom-1x">
-              <div class="col-xl-4 col-md-6 col-sm-4"><a class="btn btn-sm btn-block facebook-btn" href="#"><i class="socicon-facebook"></i>&nbsp;Facebook login</a></div>
-              <div class="col-xl-4 col-md-6 col-sm-4"><a class="btn btn-sm btn-block twitter-btn" href="#"><i class="socicon-twitter"></i>&nbsp;Twitter login</a></div>
-              <div class="col-xl-4 col-md-6 col-sm-4"><a class="btn btn-sm btn-block google-btn" href="#"><i class="socicon-googleplus"></i>&nbsp;Google+ login</a></div>
-            </div>
-            <h4 class="margin-bottom-1x">Hoặc điền thông tin vào biểu mẫu</h4>
-            <div class="form-group input-group">
-              <input name="login_username" id="login_username" tabindex="1" class="form-control" placeholder="{{ trans('frontend.frontend_username_placeholder') }}" value="{{ $frontend_login_data['user'] }}" type="text">
-              <span class="input-group-addon"><i class="icon-mail"></i></span>
-            </div>
-            <div class="form-group input-group">
-              <input name="login_password" id="login_password" tabindex="2" class="form-control" placeholder="{{ trans('frontend.password') }}" type="password" value="{{ $frontend_login_data['pass'] }}">
-              <span class="input-group-addon"><i class="icon-lock"></i></span>
-            </div>
-            <div class="d-flex flex-wrap justify-content-between padding-bottom-1x">
-              <div class="custom-control custom-checkbox">
-                @if (Cookie::has('frontend_remember_me_data'))
-                  <input class="custom-control-input" type="checkbox" id="remember_me" checked>
-                  <label class="custom-control-label" for="remember_me">{{ trans('frontend.remember_me') }}</label>
-                @else
-                  <input class="custom-control-input" type="checkbox" id="remember_me">
-                  <label class="custom-control-label" for="remember_me">{{ trans('frontend.remember_me') }}</label>
-                @endif
-
-              </div>
-              <a class="navi-link" href="{{ route('user-forgot-password-page') }}">{{ trans('frontend.forgot_password') }}</a>
-            </div>
-
-            @if($frontend_login_data['is_enable_recaptcha'] == true)
-              <div class="form-group">
-                <div class="captcha-style">{!! NoCaptcha::display() !!}</div>
-              </div>
-            @endif
-
-            <a class="navi-link" href="{{ route('user-registration-page') }}">{{ trans('frontend.frontend_user_registration_title') }}</a>
-
-
-            <div class="text-center text-sm-right">
-              <button class="btn btn-primary margin-bottom-none" type="submit" name="login_submit" id="login_submit" tabindex="4">{{ trans('frontend.frontend_log_in') }}</button>
-            </div>
+      
+        <div class="card-body">
+          <div class="row margin-bottom-1x">
+            <div class="col-xl-4 col-md-6 col-sm-4"><a class="btn btn-sm btn-block facebook-btn" href="#"><i class="socicon-facebook"></i>&nbsp;Facebook login</a></div>
+            <div class="col-xl-4 col-md-6 col-sm-4"><a class="btn btn-sm btn-block twitter-btn" href="#"><i class="socicon-twitter"></i>&nbsp;Twitter login</a></div>
+            <div class="col-xl-4 col-md-6 col-sm-4"><a class="btn btn-sm btn-block google-btn" href="#"><i class="socicon-googleplus"></i>&nbsp;Google+ login</a></div>
           </div>
-        </form>
-      </div>
+          <h4 class="margin-bottom-1x">Hoặc điền thông tin vào biểu mẫu</h4>
+          <div class="form-group input-group">
+            <input name="login_username" id="login_username" tabindex="1" class="form-control" placeholder="{{ trans('frontend.frontend_username_placeholder') }}" value="{{ $frontend_login_data['user'] }}" type="text">
+            <span class="input-group-addon"><i class="icon-mail"></i></span>
+          </div>
+          <div class="form-group input-group">
+            <input name="login_password" id="login_password" tabindex="2" class="form-control" placeholder="{{ trans('frontend.password') }}" type="password" value="{{ $frontend_login_data['pass'] }}">
+            <span class="input-group-addon"><i class="icon-lock"></i></span>
+          </div>
+          <div class="d-flex flex-wrap justify-content-between padding-bottom-1x">
+            <div class="custom-control custom-checkbox">
+              @if (Cookie::has('frontend_remember_me_data'))
+                <input class="custom-control-input" type="checkbox" id="remember_me" checked>
+                <label class="custom-control-label" for="remember_me">{{ trans('frontend.remember_me') }}</label>
+              @else
+                <input class="custom-control-input" type="checkbox" id="remember_me">
+                <label class="custom-control-label" for="remember_me">{{ trans('frontend.remember_me') }}</label>
+              @endif
 
-      
-      
+            </div>
+            <a class="navi-link" href="{{ route('user-forgot-password-page') }}">{{ trans('frontend.forgot_password') }}</a>
+          </div>
+
+          @if($frontend_login_data['is_enable_recaptcha'] == true)
+            <div class="form-group">
+              <div class="captcha-style">{!! NoCaptcha::display() !!}</div>
+            </div>
+          @endif
+
+          <a class="navi-link" href="{{ route('user-registration-page') }}">{{ trans('frontend.frontend_user_registration_title') }}</a>
+
+
+          <div class="text-center text-sm-right">
+            <button class="btn btn-primary margin-bottom-none" type="submit" name="login_submit" id="login_submit" tabindex="4">{{ trans('frontend.frontend_log_in') }}</button>
+          </div>
+        </div>
+      </form>
     </div>
+
+    
+    
   </div>
+</div>
 
 @endsection
