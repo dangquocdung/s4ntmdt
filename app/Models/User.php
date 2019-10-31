bfd2da7049c14edee51621608ff74009097bf327
dangquocdung <dungthinhvn@gmail.com>
2019-10-31 11:11:33 +0700

    asdsa

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,7 @@ class User extends Model
 {
    
   protected $table = 'users';
-  //protected $fillable = ['*'];
+  protected $fillable = ['display_name'];
   //protected $primaryKey = 'id';
   
   public function roles()
