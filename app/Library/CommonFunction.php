31bad7cb29ed2182243e8b752090e3b740a49739
dangquocdung <dungthinhvn@gmail.com>
2019-10-31 12:27:09 +0700

    aass ddd

--- app/Library/CommonFunction.php
+++ app/Library/CommonFunction.php
@@ -286,6 +286,10 @@ class CommonFunction
     $data_ary['seo_data'] = get_seo_data();
 
     $data_ary['user_info'] = get_current_frontend_user_info();
+
+    if (empty($data_ary['user_info'])){
+      Session::forget('shopist_frontend_user_id');
+    }
     
     return $data_ary;
   }
