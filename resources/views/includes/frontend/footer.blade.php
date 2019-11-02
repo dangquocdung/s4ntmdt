ecda089e691d140e1f30a33789b607a265945ced
dangquocdung <dungthinhvn@gmail.com>
2019-11-02 21:51:39 +0700

    ádsad

--- resources/views/includes/frontend/footer.blade.php
+++ resources/views/includes/frontend/footer.blade.php
@@ -79,8 +79,8 @@
                     <li><span class="opacity-50">Cơ quan chủ quản:&nbsp;</span>Sở Th&ocirc;ng tin v&agrave; Truyền th&ocirc;ng H&agrave; Tĩnh</li>
                     <li><span class="opacity-50">Cơ quan quản trị:&nbsp;</span>Trung t&acirc;m C&ocirc;ng nghệ th&ocirc;ng tin v&agrave; Truyền th&ocirc;ng H&agrave; Tĩnh</li>
                     <li><span class="opacity-50">Địa chỉ:&nbsp;</span>Số 18, đường 26/3, th&agrave;nh phố H&agrave; Tĩnh, tỉnh H&agrave; Tĩnh</li>
-                    <li><span class="opacity-50">Điện thoại:&nbsp;</span> 0239&nbsp;895589.</li>
-                    <li><span class="opacity-50">Email:&nbsp;</span><a href="mailto:ttcntt-tt@hatinh.gov.vn">ttcntt-tt@hatinh.gov.vn</a></li>
+                    <li><span class="opacity-50">Điện thoại:&nbsp;</span> 0239&nbsp;895589, <span class="opacity-50">Email:&nbsp;</span><a href="mailto:ttcntt-tt@hatinh.gov.vn">ttcntt-tt@hatinh.gov.vn</a></li>
+                    <li></li>
                 </ul>
         
                 <p class="opacity-50"><em>* Phiên bản Sàn GDTMĐT đang chạy thử nghiệm. Mọi chi tiết và yêu cầu hỗ trợ vui lòng liên hệ với ban quản trị.</em></p>
@@ -107,20 +107,20 @@
                 </form>
                 <!-- <h3 class="widget-title">Ứng dụng di động</h3> -->
 
-                <a class="market-button apple-button mb-light-skin" href="javascript:void(0)">
+                {{-- <a class="market-button apple-button mb-light-skin" href="javascript:void(0)">
                     <span class="mb-subtitle">Tải về trên</span>
                     <span class="mb-title">App Store</span>
                 </a>
                 <a class="market-button google-button mb-light-skin" href="javascript:void(0)">
                     <span class="mb-subtitle">Tải về trên</span>
                     <span class="mb-title">Google Play</span>
-                </a>
+                </a> --}}
 
             </section>
         </div>
         <div class="col-md-3">
             <section class="widget widget-light-skin">
-                <h3 class="widget-title">Đã đăng kí Bộ Công thương</h3>
+                <h3 class="widget-title">Chứng nhận</h3>
                 <a href="http://online.gov.vn/HomePage/WebsiteDisplay.aspx?DocId=19094"><img alt="" src="http://online.gov.vn/seals/2ZgbypVobA+pgtjkpLPKdw==.jpgx" style="float:left" /></a>
             </section>
         </div>
