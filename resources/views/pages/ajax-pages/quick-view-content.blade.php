789a887890594bc0de8e88e6f6b98fa96edead9f
dangquocdung <dungthinhvn@gmail.com>
2019-11-03 19:28:36 +0700

    sadsa

--- resources/views/pages/ajax-pages/quick-view-content.blade.php
+++ resources/views/pages/ajax-pages/quick-view-content.blade.php
@@ -1,77 +1,208 @@
-<div class="qwick-view-left">
-    <div class="quick-view-learg-img">
-        <div class="quick-view-tab-content tab-content">
-            <div class="tab-pane active show fade" id="modal1" role="tabpanel">
-                <img src="assets/img/quick-view/l1.jpg" alt="">
-            </div>
-            <div class="tab-pane fade" id="modal2" role="tabpanel">
-                <img src="assets/img/quick-view/l2.jpg" alt="">
-            </div>
-            <div class="tab-pane fade" id="modal3" role="tabpanel">
-                <img src="assets/img/quick-view/l3.jpg" alt="">
-            </div>
+<script type="text/javascript">
+  $(document).ready(function(){
+    $('.model__quick_view_product_info .btn-number').click(function(e){
+      e.preventDefault();
+      fieldName = $(this).attr('data-field');
+      type      = $(this).attr('data-type');
+      var input = $("input[name='"+fieldName+"']");
+      var currentVal = parseInt(input.val());
+      if (!isNaN(currentVal)) {
+          if(type == 'minus') {
+
+              if(currentVal > input.attr('min')) {
+                  input.val(currentVal - 1).change();
+              } 
+              if(parseInt(input.val()) == input.attr('min')) {
+                  $(this).attr('disabled', true);
+              }
+
+          } else if(type == 'plus') {
+
+              if(currentVal < input.attr('max')) {
+                  input.val(currentVal + 1).change();
+              }
+              if(parseInt(input.val()) == input.attr('max')) {
+                  $(this).attr('disabled', true);
+              }
+
+          }
+      } else {
+          input.val(0);
+      }
+    });
+	
+    $('.model__quick_view_product_info .input-number').focusin(function(){
+       $(this).data('oldValue', $(this).val());
+    });
+	
+    $('.model__quick_view_product_info .input-number').change(function() {
+
+        minValue =  parseInt($(this).attr('min'));
+        maxValue =  parseInt($(this).attr('max'));
+        valueCurrent = parseInt($(this).val());
+
+        name = $(this).attr('name');
+        if(valueCurrent >= minValue) {
+            $(".model__quick_view_product_info .btn-number[data-type='minus'][data-field='"+name+"']").removeAttr('disabled')
+        } else {
+            $(this).val($(this).data('oldValue'));
+        }
+        if(valueCurrent <= maxValue) {
+            $(".model__quick_view_product_info .btn-number[data-type='plus'][data-field='"+name+"']").removeAttr('disabled')
+        } else {
+            $(this).val($(this).data('oldValue'));
+        }
+    });
+	
+    $(".model__quick_view_product_info .input-number").keydown(function (e) {
+      // Allow: backspace, delete, tab, escape, enter and .
+      if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
+           // Allow: Ctrl+A
+          (e.keyCode == 65 && e.ctrlKey === true) || 
+           // Allow: home, end, left, right
+          (e.keyCode >= 35 && e.keyCode <= 39)) {
+               // let it happen, don't do anything
+               return;
+      }
+      // Ensure that it is a number and stop the keypress
+      if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+          e.preventDefault();
+      }
+    });
+    
+//    if($('.add-to-cart-bg').length>0 || $('.single-page-add-to-cart').length>0){
+//      dynamicAddToCart();
+//    }
+  });
+</script>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-xs-12 col-sm-5 col-md-5">
+      <div class="model__quick_view_product_image">
+        @if( !empty($single_product_details['post_image_url']) && $single_product_details['_product_related_images_url']->product_image != '/images/upload.png')
+          <img src="{{ get_image_url($single_product_details['post_image_url']) }}" alt="{{ basename($single_product_details['post_image_url']) }}" class="img-responsive"/>
+        @else
+          <img src="{{ default_placeholder_img_src() }}" alt="" class="img-responsive" />
+        @endif
+      </div>
+    </div>      
+
+    <div class="col-xs-12 col-sm-7 col-md-7">
+      <div class="model__quick_view_product_info">
+        <h2 class="product-title">{{ $single_product_details['post_title'] }}</h2>
+
+        <div class="rating-stars">
+          <div class="star-rating">
+            <span style="width:{{ $comments_rating_details['percentage'] }}%"></span>
+          </div>
         </div>
-    </div>
-    <div class="quick-view-list nav" role="tablist">
-        <a class="active" href="#modal1" data-toggle="tab" role="tab" aria-selected="true" aria-controls="home1">
-            <img src="assets/img/quick-view/s1.jpg" alt="">
-        </a>
-        <a href="#modal2" data-toggle="tab" role="tab" aria-selected="false" aria-controls="home2">
-            <img src="assets/img/quick-view/s2.jpg" alt="">
-        </a>
-        <a href="#modal3" data-toggle="tab" role="tab" aria-selected="false" aria-controls="home3">
-            <img src="assets/img/quick-view/s3.jpg" alt="">
-        </a>
-    </div>
-</div>
-<div class="qwick-view-right">
-    <div class="qwick-view-content">
-        <h3>Handcrafted Supper Mug</h3>
-        <div class="price">
-            <span class="new">$90.00</span>
-            <span class="old">$120.00  </span>
+
+
+
+        <div class="model__quick_view_product_price">
+          @if(get_product_type($single_product_details['id']) == 'simple_product')
+          @if($single_product_details['post_regular_price'] && $single_product_details['post_regular_price'] >0 && $single_product_details['post_sale_price'] && $single_product_details['post_sale_price']>0 && $single_product_details['post_regular_price'] > $single_product_details['post_sale_price'] )
+          <span class="offer-price">{!! price_html( get_product_price_html_by_filter(get_role_based_price_by_product_id($single_product_details['id'], $single_product_details['post_regular_price'])), get_frontend_selected_currency()) !!}</span>
+          @endif
+          <span class="solid-price">{!! price_html( get_product_price_html_by_filter(get_role_based_price_by_product_id($single_product_details['id'], $single_product_details['post_price'])), get_frontend_selected_currency()) !!}</span>
+
+          @if($single_product_details['post_regular_price'] && $single_product_details['post_sale_price'] && $single_product_details['post_regular_price'] > $single_product_details['post_sale_price'] && $single_product_details['_product_sale_price_start_date'] && $single_product_details['_product_sale_price_end_date'] && $single_product_details['_product_sale_price_end_date'] >= date("Y-m-d"))
+          <p class="offer-message-label">&#10148; {{ trans('frontend.offer_msg') }}  <i>{!! date("F j, Y", strtotime($single_product_details['_product_sale_price_start_date'])) !!} {{ trans('frontend.to') }} {!! date("F j, Y", strtotime($single_product_details['_product_sale_price_end_date'])) !!} </i></p>
+          @endif
+
+        @elseif(get_product_type($single_product_details['id']) == 'configurable_product' || get_product_type($single_product_details['id']) == 'customizable_product')
+          <span class="solid-price">{!! get_product_variations_min_to_max_price_html($currency_symbol, $single_product_details['id']) !!} </span>
+        @endif
+        </div> 
+
+        <div class="model__quick_view_product_stock">
+          @if( get_product_type($single_product_details['id']) == 'simple_product' || (get_product_type($single_product_details['id']) == 'customizable_product' && count(get_product_variations($single_product_details['id'])) == 0))
+
+            @if( $single_product_details['post_stock_availability'] == 'in_stock' || ($single_product_details['_product_manage_stock'] == 'yes' && $single_product_details['_product_manage_stock_back_to_order'] == 'only_allow' && $single_product_details['post_stock_availability'] == 'in_stock') || ($single_product_details['_product_manage_stock'] == 'yes' && $single_product_details['_product_manage_stock_back_to_order'] == 'allow_notify_customer' && $single_product_details['post_stock_availability'] == 'in_stock') )
+              <p class="availability-status"><span>{{ trans('frontend.single_product_availability_label') }}: </span> <span class="in-stock">{{ trans('frontend.single_product_availability_status_instock_label') }}</span></p>
+            @else
+              <p class="availability-status"><span>{{ trans('frontend.single_product_availability_label') }}: </span> <span class="in-stock">{{ trans('frontend.single_product_availability_status_outstock_label') }}</span></p>
+            @endif
+
+            @if( ($single_product_details['_product_manage_stock'] == 'yes' && $single_product_details['_product_manage_stock_back_to_order'] == 'only_allow' && $single_product_details['post_stock_availability'] == 'in_stock') || ($single_product_details['_product_manage_stock'] == 'yes' && $single_product_details['_product_manage_stock_back_to_order'] == 'allow_notify_customer' && $single_product_details['post_stock_availability'] == 'in_stock') )
+              <p class="availability-status"><span>{{ trans('frontend.single_product_available_stock_label') }}: </span> <span class="stock-amount">{{ $single_product_details['post_stock_qty'] }}</span></p>
+            @endif
+
+            @if($single_product_details['_product_manage_stock'] == 'yes' && $single_product_details['_product_manage_stock_back_to_order'] == 'allow_notify_customer' && $single_product_details['post_stock_availability'] == 'in_stock')
+              <p class="stock-notify-msg">&#10148; {{ trans('frontend.product_available_msg') }}</p>
+            @endif
+          @endif
+        </div> <hr>
+
+        @if($single_product_details['post_content'])
+        <p class="text-muted">
+        {!! get_limit_string(string_decode($single_product_details['post_content']),200) !!}
+        <!-- <a href='#details' class='scroll-to'>More info</a> -->
+        </p>
+        <!-- <p class="product-description">{!! get_limit_string(string_decode($single_product_details['post_content']), 200) !!}</p> -->
+        <hr>
+        @endif
+
+
+        
+
+      <div class="row align-items-end pb-4">
+        <div class="col-sm-4">
+          <div class="form-group mb-0">
+          <label for="quantity">{!! trans('frontend.quantity') !!}</label>
+
+            @php
+
+              $qty = ''; 
+
+              if($single_product_details['_product_manage_stock_back_to_order'] == 'not_allow' && $single_product_details['post_stock_qty']>0){
+                $qty = $single_product_details['post_stock_qty'];
+              }
+
+            @endphp
+
+            <select class="form-control" id="quantity" name="quant[1]">
+
+            @if ($qty > 1)
+
+              @for($i=1; $i<$qty; $i++)
+
+                <option value="{{ $i }}">{{ $i}}</option>
+
+              @endfor
+            @else
+
+              <option value="1" selected>1</option>
+
+            @endif
+
+            </select>
+          </div>
         </div>
-        <div class="rating-number">
-            <div class="quick-view-rating">
-                <i class="ion-ios-star red-star"></i>
-                <i class="ion-ios-star red-star"></i>
-                <i class="ion-android-star-outline"></i>
-                <i class="ion-android-star-outline"></i>
-                <i class="ion-android-star-outline"></i>
-            </div>
-            <div class="quick-view-number">
-                <span>2 Ratting (S)</span>
-            </div>
+        <div class="col-sm-8">
+          <div class="pt-4 hidden-sm-up"></div>
+          <button class="btn btn-primary btn-block m-0 add-to-cart-bg" data-toast data-toast-type="success" data-toast-position="topRight" data-toast-icon="icon-check-circle" data-toast-title="Sản phẩm" data-toast-message="đã thêm vào giỏ hàng!" data-id="{{ $single_product_details['id'] }}" data-toggle="tooltip" data-placement="top" title="" data-original-title="{{ trans('frontend.add_to_cart_label') }}"><i class="icon-bag"></i> Thêm vào giỏ hàng</button>
         </div>
-        <p>Lorem ipsum dolor sit amet, consectetur adip elit, sed do tempor incididun ut labore et dolore magna aliqua. Ut enim ad mi , quis nostrud veniam exercitation .</p>
-        <div class="quick-view-select">
-            <div class="select-option-part">
-                <label>Size*</label>
-                <select class="select">
-                    <option value="">- Please Select -</option>
-                    <option value="">900</option>
-                    <option value="">700</option>
-                </select>
-            </div>
-            <div class="select-option-part">
-                <label>Color*</label>
-                <select class="select">
-                    <option value="">- Please Select -</option>
-                    <option value="">orange</option>
-                    <option value="">pink</option>
-                    <option value="">yellow</option>
-                </select>
-            </div>
-        </div>
-        <div class="quickview-plus-minus">
-            <div class="cart-plus-minus">
-          <input type="text" value="02" name="qtybutton" class="cart-plus-minus-box">
-        </div>
-        <div class="quickview-btn-cart">
-            <a class="btn-hover-black" href="#">add to cart</a>
-        </div>
-        <div class="quickview-btn-wishlist">
-            <a class="btn-hover" href="#"><i class="ion-ios-heart-outline"></i></a>
-        </div>
-    </div>
+      </div>
+
+      <div class="pt-1 mb-1 store-name">
+        <span class="text-medium">{!! trans('frontend.gian-hang') !!}: </span>
+        <a href="{{ route('store-details-page-content', get_user_name_by_user_id($single_product_details['_selected_vendor'])) }}" target="_blank">{{ get_user_name_by_user_id($single_product_details['_selected_vendor']) }}</a>
+      </div>
+
+      <div class="pt-1 mb-4"><span class="text-medium">{!! trans('frontend.sku') !!}: </span>
+        @if ($single_product_details['post_sku'])
+          #{{ $single_product_details['post_sku'] }}
+        @else
+          #N/A
+        @endif
+      </div>
+
+   
+        
+
+       
+      </div>
+    </div>    
+  </div>      
 </div>
