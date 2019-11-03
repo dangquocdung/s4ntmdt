68a74351f7360b0a0eddaa34c664310dd6882498
dangquocdung <dungthinhvn@gmail.com>
2019-11-03 17:37:07 +0700

    asdsa

--- resources/views/modal/quick-view.blade.php
+++ resources/views/modal/quick-view.blade.php
@@ -1,9 +1,12 @@
 <!-- modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-hidden="true">
-  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-      <span class="ion-android-close" aria-hidden="true"></span>
-  </button>
-  <div class="modal-dialog" role="document">
-      <div class="modal-content modal__content"><div>
+<div class="shopist-modal" id="quick_view_modal">
+  <div class="modal__window">
+
+    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span class="ion-android-close" aria-hidden="true"></span>
+    </button>
+    <div class="modal-dialog" role="document">
+        <div class="modal__content"><div>
+    </div>
   </div>
 </div>
