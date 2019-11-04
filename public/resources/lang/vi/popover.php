98ffca15636100794282226f2b64b2d40bde58b3
dangquocdung <dungthinhvn@gmail.com>
2019-11-04 15:37:03 +0700

    ádsa

--- public/resources/lang/vi/popover.php
+++ public/resources/lang/vi/popover.php
@@ -2,7 +2,7 @@
 return [
     'vendor_cover_image' => 'Hình ảnh được tải lên của bạn sẽ xuất hiện tại trang chi tiết gian hàng',
     'vendor_cat_add' => 'Tìm kiếm danh mục và thêm. Các danh mục đã chọn của bạn sẽ được hiển thị tại trang chủ của gian hàng',
-    'show_map_extra_label' =>   'Bật hiển thị bản đồ vị trí cửa hàng trên thanh bên trái cửa hàng',
-    'show_contact_form_extra_label' =>  'Cho phép hiển thị biểu mẫu gian hàng liên lạc trên thanh bên trái cửa hàng',
+    'show_map_extra_label' =>   'Bật hiển thị bản đồ vị trí gian hàng trên thanh bên trái gian hàng',
+    'show_contact_form_extra_label' =>  'Cho phép hiển thị biểu mẫu gian hàng liên lạc trên thanh bên trái gian hàng',
     'vendor_commission_msg' =>  'Bao nhiêu (%) một gian hàng sẽ nhận được từ mỗi đơn hàng'
 ];
