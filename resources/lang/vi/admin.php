06eef989dc70c276774cbc19b182781d21bbd8f2
dangquocdung <dungthinhvn@gmail.com>
2019-11-27 14:21:01 +0700

    ádsa

--- resources/lang/vi/admin.php
+++ resources/lang/vi/admin.php
@@ -2,7 +2,7 @@
 return [
   'view_online_store'   =>  'Gian hàng của bạn',
   'shopist_store'       =>  'Ghé thăm gian hàng',
-  'profile'             =>  'Hồ sơ',
+  'profile'             =>  'Thông tin gian hàng',
   'sign_out'            =>  'Đăng xuất',
   'dashboard' =>'Bảng điều khiển',
   'products' =>'Quản lý sản phẩm',
