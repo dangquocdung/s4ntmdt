3ee6a3be3daca77b2b8fdc43f5bc189ef8b8dbf9
dangquocdung <dungthinhvn@gmail.com>
2019-12-12 10:08:59 +0700

    asdas

--- resources/views/includes/frontend/vendor-categories.blade.php
+++ resources/views/includes/frontend/vendor-categories.blade.php
@@ -12,11 +12,19 @@
 
           @if(count($data['children'])>0)
 
+          @if (isset($vendor_products['parent_id']))
+
             @if( $data['id'] == $vendor_products['parent_id'] )
               <li class="has-children expanded">
             @else
               <li class="has-children">
             @endif
+
+          @else
+
+            <li class="has-children">
+
+          @endif
 
               <a href="#">{!! $data['name'] !!}</a>
               <!-- <span>(123456)</span> -->
@@ -24,7 +32,11 @@
                 @if(count($data['children'])>0)
                 <ul>
                   @foreach($data['children'] as $data)
+                  @if (isset($vendor_products['selected_cat'][0]))
                     <li class="{{ ($data['id']==$vendor_products['selected_cat'][0])?'active':'' }}">
+                  @else
+                    <li>
+                  @endif
                       <a href="{{ route('store-products-cat-page-content', array($data['slug'], $user_name)) }}"> {!! $data['name'] !!} </a>
                       @include('pages.common.vendor-children-category', array('data' => $data, 'user_name' => $user_name))
                     </li>
