8d334246e52e27167318faba2c29c1d93ab67c08
dangquocdung <dungthinhvn@gmail.com>
2019-12-18 15:31:15 +0700

    get_vendor_name_by_product_id

---
+++ resources/views/emails/vendor-new-account.blade.php
@@ -0,0 +1,95 @@
+<?php $data = get_emails_option_data();?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
+<title>Sàn GDTMĐT Hà Tĩnh</title>
+</head>
+<body style="margin: 0; padding: 0;">
+
+<div style="background-color:#f0f0f0;">
+    <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="margin:0 auto;width:600px !important;min-width:600px !important;" class="yiv9635060181container">
+        <tbody>
+            <tr>
+                <td align="center" valign="middle" style="background:#ffffff;">
+                    <table style="width:580px;border-bottom:1px solid #ff3333;" cellpadding="0" cellspacing="0" border="0">
+                        <tbody>
+                            <tr>
+                                <td align="left" valign="middle" style="width:500px;min-height:60px;">
+                                    <a rel="nofollow" style="border:0;" target="_blank" href="https://hatinhtrade.com.vn/">
+                                      <img style="display:block;border:0px;width:130px;min-height:35px;" src="https://hatinhtrade.com.vn/images/logo-san.png"> 
+                                    </a>
+                                </td>
+                                <td align="right" valign="middle" style="padding-right:15px;">
+                                    <a rel="nofollow" style="border:0;"><img height="40" width="112" style="display:block;border:0px;" src="https://hatinhtrade.com.vn/images/hoan-tra-hang.png"> </a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td align="center" valign="middle" style="background:#ffffff;">
+                    <table style="width:580px;" cellpadding="0" cellspacing="0" border="0">
+                        <tbody>
+                            <tr>
+                                <td valign="middle" style="font-family:Arial, Helvetica, sans-serif;font-size:24px;color:rgb(255, 51, 51);text-transform:uppercase;font-weight:bold;padding:25px 10px 15px;text-align:center;">
+                                  Tài khoản nhà cung cấp
+                                </td>
+                            </tr>
+                            <tr>
+                              <td align="left" valign="middle" style="font-family:Arial, Helvetica, sans-serif;font-size:12px;color:#666666;padding:0 10px 20px 10px;line-height:17px;">
+                                <p>{!! trans('admin.new_account_mail_notice') !!}</p>
+                                <!-- <p>
+                                  <a href="https://hatinhtrade.com.vn/kich-hoat-tai-khoan/{{ $_confirmation_code }}"></a>https://hatinhtrade.com.vn/kich-hoat-tai-khoan-nguoi-dung/{{ $_confirmation_code }}
+                                </p> -->
+                              </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td align="center" valign="middle" style="background:#ffffff;padding-top:20px;padding-bottom:20px;">
+                    <table style="width:580px;background:#f7f7f7;" cellpadding="0" cellspacing="0" border="0">
+                        <tbody>
+                            <tr>
+                                <td align="left" valign="middle" style="font-family:Arial, Helvetica, sans-serif;font-size:11px;color:#ff3333;padding-left:8px;">
+                                  <strong>CÀI ĐẶT ỨNG DỤNG&nbsp;SÀN GDTMĐT HÀ TĨNH</strong>
+                                </td>
+                                <td align="right" valign="middle" style="width:190px;">
+                                    <a rel="nofollow" style="border:0px;text-decoration:none;" target="_blank" href="#"><img height="34" width="83" style="border:0px;" src="https://hatinhtrade.com.vn/images/appstore.jpg"> </a>
+                                    <a rel="nofollow" style="border:0px;text-decoration:none;padding-left:3px;" target="_blank" href="#"> <img height="34" width="83" style="border:0px;" src="https://hatinhtrade.com.vn/images/gg-play.jpg"> </a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td align="left" valign="top">
+                    <table style="width:600px;padding:20px 0 0 0;" cellpadding="0" cellspacing="0" border="0">
+                        <tbody>
+                            <tr>
+                                <td align="left" valign="top" width="49" style="width:49px;padding-left:10px;">
+                                    <a rel="nofollow" style="border:0;" target="_blank" href="https://hatinhtrade.com.vn/">
+                                      <img height="50" width="49" style="display:block;border:0px;" src="https://hatinhtrade.com.vn/images/logo-app.png"> 
+                                    </a>
+                                </td>
+                                <td align="left" width="390" valign="top" style="font-family:Arial, Helvetica, sans-serif;font-size:11px;color:#3c3c3c;padding:0 0 0 10px;line-height:17px;width:390px;">Trung tâm CNTT & Truyền thông Hà Tĩnh
+                                    <br>Số 18 đường 26-3, Phường Nam Hà,
+                                    <br>TP. Hà Tĩnh, tỉnh Hà Tĩnh.</td>
+                                <td align="right" valign="top" width="131" style="padding-right:10px;">
+                                  <img height="42" width="131" style="display:block;border:0px;" src="https://hatinhtrade.com.vn/images/da-dang-ky-bct.jpg">
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+</body>
+</html>
