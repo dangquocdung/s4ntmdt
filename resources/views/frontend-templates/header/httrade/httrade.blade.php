b2fae4d92be37068e0769e3edb04fb696fa11893
dangquocdung <dungthinhvn@gmail.com>
2020-04-11 14:27:19 +0700

    asdsad

--- resources/views/frontend-templates/header/httrade/httrade.blade.php
+++ resources/views/frontend-templates/header/httrade/httrade.blade.php
@@ -2,7 +2,7 @@
 <div class="topbar d-flex justify-content-between">
   <!-- Logo-->
   <div class="site-branding d-flex">
-    <a class="site-logo align-self-center" href="{{ route('home-page') }}">
+    <a class="site-logo align-self-center" href="{{ route('home-page') }}" style="width:200px">
 
     @if(get_site_logo_image())      
       <img src="{{ get_site_logo_image() }}" alt="{{ trans('frontend.your_store_label') }}">
