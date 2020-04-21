2f2ea3f4a9dae4020cc4bc8caca5a0413c6e5f43
dangquocdung <dungthinhvn@gmail.com>
2020-04-21 14:39:36 +0700

    asdsad

--- resources/views/pages/admin/settings/appearance-content.blade.php
+++ resources/views/pages/admin/settings/appearance-content.blade.php
@@ -369,15 +369,20 @@
 
                     <div class="uploaded-header-slider-images">
                       @if(count(get_appearance_header_settings_data()) > 0 )
-                      <div class="sample-img" style="display:none;"><img class="upload-icon" src="{{ default_upload_sample_img_src() }}"></div>
-                      <div class="uploaded-slider-images" style="display:block;">
-                        @foreach(get_appearance_header_settings_data() as $slider_img)
-                          <div class="header-slider-image-single-container {{ substr(basename($slider_img->img_url), 0, -4) }}"><img src="{{ get_image_url($slider_img->img_url) }}"><div data-id="{{ $slider_img->id }}" class="remove-frontend-img-link" style="display: none;"></div></div>
-                        @endforeach
-                      </div>
+                        <div class="sample-img" style="display:none;">
+                          <img class="upload-icon" src="{{ default_upload_sample_img_src() }}">
+                        </div>
+                        <div class="uploaded-slider-images" style="display:block;">
+                          @foreach(get_appearance_header_settings_data() as $slider_img)
+                            <div class="header-slider-image-single-container {{ substr(basename($slider_img->img_url), 0, -4) }}">
+                              <img src="{{ get_image_url($slider_img->img_url) }}">
+                              <div data-id="{{ $slider_img->id }}" class="remove-frontend-img-link" style="display: none;"></div>
+                            </div>
+                          @endforeach
+                        </div>
                       @else
-                      <div class="sample-img"><img class="upload-icon" src="{{ default_upload_sample_img_src() }}"></div>
-                      <div class="uploaded-slider-images"></div>
+                        <div class="sample-img"><img class="upload-icon" src="{{ default_upload_sample_img_src() }}"></div>
+                        <div class="uploaded-slider-images"></div>
                       @endif
                     </div>
                   </div>
