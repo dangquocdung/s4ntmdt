654fd6ed718601cc394d03719bc4334a5e03a2cd
dangquocdung <dungthinhvn@gmail.com>
2020-04-23 12:18:54 +0700

    asdsad

--- resources/views/frontend-templates/single-product/httrade/httrade.blade.php
+++ resources/views/frontend-templates/single-product/httrade/httrade.blade.php
@@ -587,27 +587,9 @@
                             </div>
                         </div>
                         <div class="product-content">
-                                <div class="product-title">
-                                    <h4><a href="{{ route('details-page', $item['post_slug']) }}">{!! $item['post_title'] !!}</a></h4>
-                                </div>
-                                <div class="product-price">
-                                    <span>
-                                        {!! price_html( get_product_price_html_by_filter(get_role_based_price_by_product_id($item['id'], $item['post_price'])), get_frontend_selected_currency()) !!}
-                                    </span>
-                                </div>
-                            </div>
-                            <div class="product-cart-categori">
-                                <div class="product-categori">
-                                    <span>{{ get_user_name_by_user_id($item['author_id']) }}</span>
-                                </div>
-                                <div class="product-cart">
-                                  <span>
-                                      <del>
-                                        {!! price_html( get_product_price_html_by_filter(get_role_based_price_by_product_id($item['id'], $item['post_regular_price'])), get_frontend_selected_currency()) !!}
-                                      </del>
-                                  </span>
-                                </div>
-                            </div>
+                          <div class="product-title">
+                              <h4><a href="{{ route('details-page', $item['post_slug']) }}">{!! $item['post_title'] !!}</a></h4>
+                          </div>
                         </div>
                     </div>
                   @endforeach
