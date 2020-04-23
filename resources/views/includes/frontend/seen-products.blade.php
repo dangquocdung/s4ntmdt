79ea7449a5a1e9116118b86b63852515e5056e10
dangquocdung <dungthinhvn@gmail.com>
2020-04-24 00:21:54 +0700

    sdsadsad

--- resources/views/includes/frontend/seen-products.blade.php
+++ resources/views/includes/frontend/seen-products.blade.php
@@ -9,7 +9,7 @@
       <div class="tab-pane fade show active" id="sp_daxem" role="tabpanel">
 
           <!-- Carousel-->
-          <div class="owl-carousel" data-owl-carousel="{ &quot;nav&quot;: false, &quot;dots&quot;: true, &quot;margin&quot;: 30, &quot;responsive&quot;: {&quot;0&quot;:{&quot;items&quot;:1},&quot;576&quot;:{&quot;items&quot;:2},&quot;768&quot;:{&quot;items&quot;:3},&quot;991&quot;:{&quot;items&quot;:3},&quot;1200&quot;:{&quot;items&quot;:4}} }">
+          <div class="owl-carousel" data-owl-carousel="{ &quot;nav&quot;: false, &quot;dots&quot;: true, &quot;margin&quot;: 30, &quot;responsive&quot;: {&quot;0&quot;:{&quot;items&quot;:2},&quot;576&quot;:{&quot;items&quot;:3},&quot;768&quot;:{&quot;items&quot;:4},&quot;991&quot;:{&quot;items&quot;:4},&quot;1200&quot;:{&quot;items&quot;:5}} }">
               
               <!-- Product-->
               @foreach($seen_items as $item)
