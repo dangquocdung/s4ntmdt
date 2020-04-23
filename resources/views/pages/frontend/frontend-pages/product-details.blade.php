1bf554835e9a1407ea5bc227bcd8c0e951d2d442
dangquocdung <dungthinhvn@gmail.com>
2020-04-23 09:08:58 +0700

    asdsad

--- resources/views/pages/frontend/frontend-pages/product-details.blade.php
+++ resources/views/pages/frontend/frontend-pages/product-details.blade.php
@@ -6,16 +6,25 @@
     <meta property="og:url" content="{{ Request::url() }}" />
     <meta property="og:type" content="article" />
     <meta property="og:title" content="{{ $single_product_details['post_title'] }}" />
-    <meta property="og:description" content="{{ get_image_url( $single_product_details['post_image_url'] ) }}" />
-    <meta property="og:image" itemprop="image" content="{!! string_decode($single_product_details['post_content']) !!}"/>
+    <meta property="og:description" content="{{ string_decode($single_product_details['post_content']) }}" />
+
+    @if(count($single_product_details['_product_related_images_url']->product_gallery_images) > 0)
+
+      @foreach($single_product_details['_product_related_images_url']->product_gallery_images as $key => $row)
+
+          <meta property="og:image" itemprop="image" content="{{ get_image_url($row->url) }}"/>
+                    
+      @endforeach
+
+    @else
+      <meta property="og:image" itemprop="image" content="{!! get_image_url( $single_product_details['post_image_url'] ) !!}"/>
+
+    @endif
 
 @stop
-
 
 @section('content')
   <div id="product_single_page">
     @include( 'frontend-templates.single-product.' .$appearance_settings['single_product']. '.' .$appearance_settings['single_product'] )
   </div>
 @endsection
-
-
