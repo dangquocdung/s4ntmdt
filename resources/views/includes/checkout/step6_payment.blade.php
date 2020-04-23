c393dd7948d0bfe6fe73da5ced2a094659204d1d
dangquocdung <dungthinhvn@gmail.com>
2020-04-23 13:04:05 +0700

    asdsad

--- resources/views/includes/checkout/step6_payment.blade.php
+++ resources/views/includes/checkout/step6_payment.blade.php
@@ -31,10 +31,13 @@
                                 </td>
                                 <td class="align-middle">
                                     <span class="text-gray-dark">{{ $payment_method_data['bacs']['method_title'] }}</span><br>
-                                    <span class="text-muted text-sm">{!! $payment_method_data['bacs']['method_description'] !!}</span>
-                                    <br>
+                                    <span class="text-muted text-sm">{!! $payment_method_data['bacs']['method_description'] !!}</span><br>
                                     <span class="text-gray-dark"><strong>Tên tài khoản:&nbsp;</strong>{{ $payment_method_data['bacs']['account_details']['account_name'] }}</span><br>
-
+                                    <span class="text-gray-dark"><strong>Số tài khoản:&nbsp;</strong>{{ $payment_method_data['bacs']['account_details']['account_name'] }}</span><br>
+                                    <span class="text-gray-dark"><strong>Tên ngân hàng:&nbsp;</strong>{{ $payment_method_data['bacs']['account_details']['account_name'] }}</span><br>
+                                    <span class="text-gray-dark"><strong>Chi nhánh:&nbsp;</strong>{{ $payment_method_data['bacs']['account_details']['account_name'] }}</span><br>
+                                    <span class="text-gray-dark"><strong>Mã IBAN:&nbsp;</strong>{{ $payment_method_data['bacs']['account_details']['account_name'] }}</span><br>
+                                    <span class="text-gray-dark"><strong>Mã SWIFT:&nbsp;</strong>{{ $payment_method_data['bacs']['account_details']['account_name'] }}</span><br>
                                 </td>
                             </tr>
                         @endif
