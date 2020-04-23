f73abdc185e00d69abde63cd58c138e596ee26fb
dangquocdung <dungthinhvn@gmail.com>
2020-04-23 23:20:39 +0700

    sadsad

--- resources/views/pages/frontend/frontend-pages/blog-single-page.blade.php
+++ resources/views/pages/frontend/frontend-pages/blog-single-page.blade.php
@@ -69,9 +69,9 @@
         <div class="pb-2" id="share-content">
           <span class="d-inline-block align-middle text-sm text-muted">{{ trans('frontend.share') }}:&nbsp;&nbsp;&nbsp;</span>
           <a class="social-button shape-rounded sb-facebook" href="//" data-toggle="tooltip" data-placement="top" title="Facebook" data-name="fb"><i class="socicon-facebook"></i></a>
-          <a class="social-button shape-rounded sb-twitter" href="//" data-toggle="tooltip" data-placement="top" title="Twitter" data-name="tweet"><i class="socicon-twitter"></i></a>
+          {{-- <a class="social-button shape-rounded sb-twitter" href="//" data-toggle="tooltip" data-placement="top" title="Twitter" data-name="tweet"><i class="socicon-twitter"></i></a>
           <a class="social-button shape-rounded sb-instagram" href="//" data-toggle="tooltip" data-placement="top" title="Instagram" data-name="insta"><i class="socicon-instagram"></i></a>
-          <a class="social-button shape-rounded sb-google-plus" href="//" data-toggle="tooltip" data-placement="top" title="Google +" data-name="gplus"><i class="socicon-googleplus"></i></a>
+          <a class="social-button shape-rounded sb-google-plus" href="//" data-toggle="tooltip" data-placement="top" title="Google +" data-name="gplus"><i class="socicon-googleplus"></i></a> --}}
         </div>
       </div>
 
