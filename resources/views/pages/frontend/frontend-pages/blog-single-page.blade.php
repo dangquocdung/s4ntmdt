f7dd0a12da43b62125c5d0462f72b18228f6db6d
dangquocdung <dungthinhvn@gmail.com>
2020-04-24 00:46:57 +0700

    sadsad

--- resources/views/pages/frontend/frontend-pages/blog-single-page.blade.php
+++ resources/views/pages/frontend/frontend-pages/blog-single-page.blade.php
@@ -107,10 +107,10 @@
 
             <div class="tab-content another-product-style jump">
                 <div class="tab-pane fade show active" id="latest_products" role="tabpanel">
-                  <div class="owl-carousel" data-owl-carousel="{ &quot;nav&quot;: false, &quot;dots&quot;: true, &quot;margin&quot;: 30, &quot;responsive&quot;: {&quot;0&quot;:{&quot;items&quot;:2},&quot;576&quot;:{&quot;items&quot;:3},&quot;768&quot;:{&quot;items&quot;:4},&quot;991&quot;:{&quot;items&quot;:5},&quot;1200&quot;:{&quot;items&quot;:5}} }">
+                  <div class="owl-carousel" data-owl-carousel="{ &quot;nav&quot;: false, &quot;dots&quot;: true, &quot;margin&quot;: 30, &quot;responsive&quot;: {&quot;0&quot;:{&quot;items&quot;:2},&quot;576&quot;:{&quot;items&quot;:3},&quot;768&quot;:{&quot;items&quot;:4},&quot;991&quot;:{&quot;items&quot;:4},&quot;1200&quot;:{&quot;items&quot;:5}} }">
                     @foreach($advanced_data['latest_items'] as $row)
                       <div class="single-product">
-                          <div class="product-img" style="height:130px">
+                          <div class="product-img" style="height:120px">
                               <a href="{{ route('blog-single-page', $row['post_slug']) }}">
 
                                 @if(!empty($row['blog_image']))  
@@ -151,14 +151,14 @@
 
             <div class="tab-content another-product-style jump">
                 <div class="tab-pane fade show active" id="latest_products" role="tabpanel">
-                  <div class="owl-carousel" data-owl-carousel="{ &quot;nav&quot;: false, &quot;dots&quot;: true, &quot;margin&quot;: 30, &quot;responsive&quot;: {&quot;0&quot;:{&quot;items&quot;:2},&quot;576&quot;:{&quot;items&quot;:3},&quot;768&quot;:{&quot;items&quot;:4},&quot;991&quot;:{&quot;items&quot;:5},&quot;1200&quot;:{&quot;items&quot;:5}} }">
+                  <div class="owl-carousel" data-owl-carousel="{ &quot;nav&quot;: false, &quot;dots&quot;: true, &quot;margin&quot;: 30, &quot;responsive&quot;: {&quot;0&quot;:{&quot;items&quot;:2},&quot;576&quot;:{&quot;items&quot;:3},&quot;768&quot;:{&quot;items&quot;:4},&quot;991&quot;:{&quot;items&quot;:4},&quot;1200&quot;:{&quot;items&quot;:5}} }">
 
 
 
                     @foreach($advanced_data['best_items'] as $row)
 
                       <div class="single-product">
-                          <div class="product-img" style="height:130px">
+                          <div class="product-img" style="height:120px">
                               <a href="{{ route('blog-single-page', $row['post_slug']) }}">
 
                                 @if(!empty($row['blog_image']))  
