e680745f11f8ec798e81bd56d9f956be604faee4
dangquocdung <dungthinhvn@gmail.com>
2020-04-23 23:31:02 +0700

    sdfdsf

---
+++ resources/views/includes/frontend/footer.blade copy 3.php
@@ -0,0 +1,112 @@
+<footer class="site-footer" style="background-image: url(/img/footer-bg.png);">
+
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <!-- Categories-->
+            <section class="widget widget-links widget-light-skin">
+                <h3 class="widget-title">Danh mục sản phẩm</h3>
+                <div class="row">
+                    <div class="col-md-6">
+                        <ul>
+                        @foreach($productCategoriesTree as $cat)
+                            @if ($loop->iteration < 6)
+                            <li>
+                                <a href="{{ route('categories-page', $cat['slug']) }}">{!! $cat['name'] !!}</a>
+                            </li>
+                            @endif
+                        @endforeach
+                        </ul>
+                    </div>
+                    <div class="col-md-6">
+                        <ul>
+                        @foreach($productCategoriesTree as $cat)
+                            @if ($loop->iteration >= 6)
+                            <li>
+                                <a href="{{ route('categories-page', $cat['slug']) }}">{!! $cat['name'] !!}</a>
+                            </li>
+                            @endif
+                        @endforeach
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        </div>
+
+        @if(count($pages_list) > 0)
+
+        <div class="col-6">
+            <!-- Categories-->
+            <section class="widget widget-links widget-light-skin">
+                <h3 class="widget-title">Hỗ trợ khách hàng</h3>
+                <div class="row">
+                    <div class="col-md-6">
+                        <ul>
+                        @foreach($pages_list as $pages)
+                            @if ($loop->iteration < 6)
+                                <li> <a href="{{ route('custom-page-content', $pages['post_slug']) }}">{!! $pages['post_title'] !!}</a></li>
+                            @endif
+
+                        @endforeach
+                        </ul>
+                    </div>
+                    <div class="col-md-6">
+                        <ul>
+                        @foreach($pages_list as $pages)
+                            @if ($loop->iteration >= 6)
+                                <li> <a href="{{ route('custom-page-content', $pages['post_slug']) }}">{!! $pages['post_title'] !!}</a></li>
+                            @endif
+                        @endforeach
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        </div>
+
+        @endif
+
+    </div>
+    {{-- <hr class="hr-light mt-2 margin-bottom-2x hidden-md-down"> --}}
+    <div class="row">
+        <div class="col-md-6">
+            <!-- Contact Info-->
+            <section class="widget widget-light-skin">
+
+                <h3 class="widget-title">Thông tin liên hệ</h3>
+
+                {!! $appearance_all_data['footer_details']['footer_about_us_description'] !!}
+
+                <!-- <ul class="list-unstyled text-sm text-white">
+                    <li><span class="opacity-50">MST/ĐKKD/QĐTL:&nbsp;</span>Số 1889/QĐ-UBND</li>
+                    <li><span class="opacity-50">Cơ quan chủ quản:&nbsp;</span>Sở Th&ocirc;ng tin v&agrave; Truyền th&ocirc;ng H&agrave; Tĩnh</li>
+                    <li><span class="opacity-50">Cơ quan quản trị:&nbsp;</span>Trung t&acirc;m C&ocirc;ng nghệ th&ocirc;ng tin v&agrave; Truyền th&ocirc;ng H&agrave; Tĩnh</li>
+                    <li><span class="opacity-50">Địa chỉ:&nbsp;</span>Số 18, đường 26/3, th&agrave;nh phố H&agrave; Tĩnh, tỉnh H&agrave; Tĩnh</li>
+                    <li><span class="opacity-50">Điện thoại:&nbsp;</span> 0239&nbsp;895589,&nbsp;&nbsp;&nbsp;<span class="opacity-50">Email:&nbsp;</span><a href="mailto:ttcntt-tt@hatinh.gov.vn">ttcntt-tt@hatinh.gov.vn</a></li>
+                    <li><em>* Phiên bản Sàn GDTMĐT đang chạy thử nghiệm. Mọi chi tiết và yêu cầu hỗ trợ vui lòng liên hệ với ban quản trị.</em></li>
+                </ul> -->
+            </section>
+        </div>
+        <!-- <div class="col-md-3">
+            <section class="widget widget-light-skin">
+                <h3 class="widget-title">Ứng dụng di động</h3>
+                <a class="market-button apple-button mb-light-skin" href="#">
+                    <span class="mb-subtitle">Tải về từ</span><span class="mb-title">App Store</span>
+                </a>
+                <a class="market-button google-button mb-light-skin" href="#">
+                    <span class="mb-subtitle">Tải về từ</span><span class="mb-title">Google Play</span>
+                </a>
+
+            </section>
+        </div> -->
+    
+        <div class="col-md-6">
+            <section class="widget widget-light-skin">
+                <h3 class="widget-title">Chứng nhận</h3>
+                <a href="{{ URL::to('http://online.gov.vn/HomePage/WebsiteDisplay.aspx?DocId=19094') }}" target="_blank">
+                    <img src="{{ URL::asset('/images/dadangky.jpgx') }}"/>
+                </a>
+            </section>
+        </div>
+    </div>
+</div>
+</footer>
