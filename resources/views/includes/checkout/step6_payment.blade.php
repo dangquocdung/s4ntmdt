fac813c28c39a648aa21a4523200479d14221698
dangquocdung <dungthinhvn@gmail.com>
2020-04-23 13:11:35 +0700

    asdasdsad

--- resources/views/includes/checkout/step6_payment.blade.php
+++ resources/views/includes/checkout/step6_payment.blade.php
@@ -31,11 +31,8 @@
                                 </td>
                                 <td class="align-middle">
                                     <span class="text-gray-dark">{{ $payment_method_data['bacs']['method_title'] }}</span><br>
-                                    <span class="text-muted text-sm"><small>{!! $payment_method_data['bacs']['method_description'] !!}</small></span><br>
-                                </td>
-                            </tr>
-                            <tr class="align-middle">
-                                <td class="align-middle">
+                                    <span class="text-muted text-sm"><small>{!! $payment_method_data['bacs']['method_description'] !!}</small></span>
+                                    <hr>
                                     <span class="text-gray-dark">Tên tài khoản:&nbsp;<strong>{{ $payment_method_data['bacs']['account_details']['account_name'] }}</strong></span><br>
                                     <span class="text-gray-dark">Số tài khoản:&nbsp;<strong>{{ $payment_method_data['bacs']['account_details']['account_number'] }}</strong></span><br>
                                     <span class="text-gray-dark">Tên ngân hàng:&nbsp;<strong>{{ $payment_method_data['bacs']['account_details']['bank_name'] }}</strong></span><br>
@@ -43,8 +40,10 @@
                                     <span class="text-gray-dark">Mã IBAN:&nbsp;<strong>{{ $payment_method_data['bacs']['account_details']['iban'] }}</strong></span><br>
                                     <span class="text-gray-dark">Mã SWIFT:&nbsp;<strong>{{ $payment_method_data['bacs']['account_details']['swift'] }}</strong></span><br>
 
+
                                 </td>
                             </tr>
+                        
                         @endif
         
         
