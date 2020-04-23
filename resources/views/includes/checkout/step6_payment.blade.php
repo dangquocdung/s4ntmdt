a1ff5cdf65dfb881b213db1ca0b1195e5ac41db9
dangquocdung <dungthinhvn@gmail.com>
2020-04-23 12:57:36 +0700

    asdsad

--- resources/views/includes/checkout/step6_payment.blade.php
+++ resources/views/includes/checkout/step6_payment.blade.php
@@ -19,29 +19,21 @@
                                 </td>
                                 <td class="align-middle">
                                     <span class="text-gray-dark">{{ $payment_method_data['cod']['method_title'] }}</span><br>
-                                    <span class="text-muted text-sm">{{ $payment_method_data['cod']['method_description'] }}</span>
+                                    <span class="text-muted text-sm">{!! $payment_method_data['cod']['method_description'] !!}</span>
                                 </td>
                             </tr>
                         @endif
 
-                        @if($payment_method_data['bacs']['enable_option'] == 'yes' && $is_user_login == true )
-                            <tr>
-                                <td class="align-middle">
-                                    <input type="radio" class="shopist-iCheck" name="payment_option" value="bacs"> 
-                                </td>
-                                <td class="align-middle">
-                                    <span class="text-gray-dark">{{ $payment_method_data['bacs']['method_title'] }}</span><br>
-                                    <span class="text-muted text-sm">{{ $payment_method_data['bacs']['method_description'] }}</span>
-                                </td>
-                            </tr>
-                        @elseif($payment_method_data['bacs']['enable_option'] == 'yes')
+                        @if($payment_method_data['bacs']['enable_option'] == 'yes')
                             <tr>
                                 <td class="align-middle">
                                     <input type="radio" class="shopist-iCheck" name="payment_option" disabled> 
                                 </td>
                                 <td class="align-middle">
                                     <span class="text-gray-dark">{{ $payment_method_data['bacs']['method_title'] }}</span><br>
-                                    <span class="text-muted text-sm">{{ $payment_method_data['bacs']['method_description'] }}</span>
+                                    <span class="text-muted text-sm">{!! $payment_method_data['bacs']['method_description'] !!}</span>
+                                    <span class="text-gray-dark">Tên tài khoản:&nbsp;{{ $payment_method_data['bacs']['method_title']['account_details']['account_name'] }}</span><br>
+
                                 </td>
                             </tr>
                         @endif
@@ -54,7 +46,7 @@
                             </td>
                             <td class="align-middle">
                                 <span class="text-gray-dark">{{ $payment_method_data['paypal']['method_title'] }}</span><br>
-                                <span class="text-muted text-sm">{{ $payment_method_data['paypal']['method_description'] }}</span>
+                                <span class="text-muted text-sm">{!! $payment_method_data['paypal']['method_description'] !!}</span>
                             </td>
                         </tr>
                         @endif
