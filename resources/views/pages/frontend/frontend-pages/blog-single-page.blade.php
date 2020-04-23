f19a1ccc5ac2dfbb4094e19de5dbd31f8ca9e78e
dangquocdung <dungthinhvn@gmail.com>
2020-04-24 00:45:05 +0700

    sadsad

--- resources/views/pages/frontend/frontend-pages/blog-single-page.blade.php
+++ resources/views/pages/frontend/frontend-pages/blog-single-page.blade.php
@@ -110,7 +110,7 @@
                   <div class="owl-carousel" data-owl-carousel="{ &quot;nav&quot;: false, &quot;dots&quot;: true, &quot;margin&quot;: 30, &quot;responsive&quot;: {&quot;0&quot;:{&quot;items&quot;:2},&quot;576&quot;:{&quot;items&quot;:3},&quot;768&quot;:{&quot;items&quot;:4},&quot;991&quot;:{&quot;items&quot;:5},&quot;1200&quot;:{&quot;items&quot;:5}} }">
                     @foreach($advanced_data['latest_items'] as $row)
                       <div class="single-product">
-                          <div class="product-img" style="height:120px">
+                          <div class="product-img" style="height:130px">
                               <a href="{{ route('blog-single-page', $row['post_slug']) }}">
 
                                 @if(!empty($row['blog_image']))  
@@ -158,7 +158,7 @@
                     @foreach($advanced_data['best_items'] as $row)
 
                       <div class="single-product">
-                          <div class="product-img" style="height:120px">
+                          <div class="product-img" style="height:130px">
                               <a href="{{ route('blog-single-page', $row['post_slug']) }}">
 
                                 @if(!empty($row['blog_image']))  
