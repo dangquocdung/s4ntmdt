eb9586521caf5674de58eca09c2517ff9eb94d40
dangquocdung <dungthinhvn@gmail.com>
2020-04-27 11:59:50 +0700

    sdfdsf

--- resources/views/pages/ajax-pages/cart-html.blade.php
+++ resources/views/pages/ajax-pages/cart-html.blade.php
@@ -116,13 +116,14 @@
         @include('pages.ajax-pages.cart-total-html')
 
         <div class="shopping-cart-footer">
-
-          <div class="row">
-              <div class="coupon-form apply-coupon">
-                <input type="text" class="form-control form-control-sm" id="apply_coupon_code" name="apply_coupon" placeholder="{{ trans('frontend.coupon_code_placeholder_text') }}">
-                <button class="btn btn-outline-primary btn-sm" name="apply_coupon_post" id="apply_coupon_post">{!! trans('frontend.apply_coupon_label') !!}</button>
-                <div class="clearfix visible-xs"></div>
-              </div>
+          <div class="column">
+            <div class="row">
+                <div class="coupon-form apply-coupon">
+                  <input type="text" class="form-control form-control-sm" id="apply_coupon_code" name="apply_coupon" placeholder="{{ trans('frontend.coupon_code_placeholder_text') }}">
+                  <button class="btn btn-outline-primary btn-sm" name="apply_coupon_post" id="apply_coupon_post">{!! trans('frontend.apply_coupon_label') !!}</button>
+                  <div class="clearfix visible-xs"></div>
+                </div>
+            </div>
           </div>
 
         </div>
