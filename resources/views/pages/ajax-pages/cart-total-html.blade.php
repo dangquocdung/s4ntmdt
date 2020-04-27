b0ebc4672c2b962bfd8d090766295197a2ddd572
dangquocdung <dungthinhvn@gmail.com>
2020-04-27 13:33:50 +0700

    ádsd

--- resources/views/pages/ajax-pages/cart-total-html.blade.php
+++ resources/views/pages/ajax-pages/cart-total-html.blade.php
@@ -8,6 +8,8 @@
       </td>
     </tr>
 
+    <?php $str='' ?>
+
     @if((!$shipping_data['shipping_option']['enable_shipping']) || ($shipping_data['shipping_option']['enable_shipping'] && !$shipping_data['flat_rate']['enable_option'] && !$shipping_data['free_shipping']['enable_option'] && !$shipping_data['local_delivery']['enable_option']))
 
     <tr>
