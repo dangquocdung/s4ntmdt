2a2bd0d64ba11ff422388da369d2b04202562f6a
dangquocdung <dungthinhvn@gmail.com>
2020-04-27 16:38:47 +0700

    sdfdsf

--- resources/views/pages/ajax-pages/cart-total-html.blade.php
+++ resources/views/pages/ajax-pages/cart-total-html.blade.php
@@ -1,4 +1,3 @@
-{!! Session::get('eBazar_shipping_method') !!}
 
 <section class="widget widget-order-summary" id="cart_page">
   <h5 class="widget-title">{!! trans('frontend.order_summary') !!}:{{ Cart::getShippingMethod()['shipping_method'] }}</h5>
