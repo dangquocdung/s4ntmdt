9dfe290ba5b19a7090f2583b4a15637b283c0d95
dangquocdung <dungthinhvn@gmail.com>
2020-04-27 16:05:12 +0700

    asdsad

--- resources/views/pages/ajax-pages/cart-total-html.blade.php
+++ resources/views/pages/ajax-pages/cart-total-html.blade.php
@@ -130,7 +130,7 @@
     </tr>
     <tr class="cart-grand-total">
       <td></td>
-      <td class="text-lg text-gray-dark" lass="value">{!! price_html( get_product_price_html_by_filter(Cart::getCartTotal()), get_frontend_selected_currency() ) !!}</td>
+      <td class="text-lg text-gray-dark value">{!! price_html( get_product_price_html_by_filter(Cart::getCartTotal()), get_frontend_selected_currency() ) !!}</td>
     </tr>
   </table>
 </section>
