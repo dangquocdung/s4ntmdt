085120f09982467104810704883052c823cca6f5
dangquocdung <dungthinhvn@gmail.com>
2020-04-27 12:01:15 +0700

    asdsad

--- resources/views/pages/ajax-pages/cart-html.blade.php
+++ resources/views/pages/ajax-pages/cart-html.blade.php
@@ -126,6 +126,9 @@
             </div>
           </div>
 
+          <div class="column text-lg">{!! trans('frontend.cart_sub_total') !!}: <span class="text-medium">{!! price_html( get_product_price_html_by_filter(Cart::getTotal()), get_frontend_selected_currency() ) !!}</span></div>
+
+
         </div>
 
         <div class="shopping-cart-footer">
