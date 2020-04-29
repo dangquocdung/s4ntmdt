89e46f19b59e0209cef437192f45da626d1e65f7
dangquocdung <dungthinhvn@gmail.com>
2020-04-29 10:49:23 +0700

    ádasd

--- resources/views/layouts/frontend/master.blade.php
+++ resources/views/layouts/frontend/master.blade.php
@@ -363,6 +363,21 @@
 
     </script>
 
+<script>
+    if ('serviceWorker' in navigator ) {
+        window.addEventListener('load', function() {
+            navigator.serviceWorker.register('./service-worker.js').then(function(registration) {
+                // Registration was successful
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }, function(err) {
+                // registration failed :(
+                console.log('ServiceWorker registration failed: ', err);
+            });
+        });
+    }
+</script>
+
+
     @if( Request::is('/') )
       <style>
         .fb_dialog.fb_dialog_advanced {
