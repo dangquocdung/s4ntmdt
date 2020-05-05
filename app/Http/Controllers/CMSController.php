6374baa1ebd3f94a11ee885e46f55e0accbbb975
dangquocdung <dungthinhvn@gmail.com>
2020-05-05 14:00:14 +0700

    asdsad

--- app/Http/Controllers/CMSController.php
+++ app/Http/Controllers/CMSController.php
@@ -1040,10 +1040,10 @@ class CMSController extends Controller
   public function get_latest_blogs($post_id = null){
     $blog_posts_array = array();
     if($post_id && $post_id > 0){
-      $posts = Post::where(['post_type' => 'post-blog', 'post_status' => 1])->where('id', '!=', $post_id)->orderBy('id', 'desc')->take(6)->get()->makeHidden('post_content')->toArray();
+      $posts = Post::where(['post_type' => 'post-blog', 'post_status' => 1])->where('id', '!=', $post_id)->orderBy('post_date', 'desc')->take(6)->get()->makeHidden('post_content')->toArray();
     }
     else{
-      $posts = Post::where(['post_type' => 'post-blog', 'post_status' => 1])->orderBy('id', 'desc')->take(6)->get()->makeHidden('post_content')->toArray();
+      $posts = Post::where(['post_type' => 'post-blog', 'post_status' => 1])->orderBy('post_date', 'desc')->take(6)->get()->makeHidden('post_content')->toArray();
     }
     
     if(count($posts) > 0){
