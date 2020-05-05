9e927c6061131103f5dd0d730850fc7e7ebb06d2
dangquocdung <dungthinhvn@gmail.com>
2020-05-05 14:39:54 +0700

    ádasd

---
+++ app/Models/PostArchive.php
@@ -0,0 +1,10 @@
+<?php
+namespace shopist\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PostArchive extends Model
+{
+  protected $table = 'post_archives';
+  // protected $hidden = ['post_content'];
+}
