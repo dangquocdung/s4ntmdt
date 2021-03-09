130a0ddc98d892aeb86a3eda165c5092d5a0f0f2
dangquocdung <dungthinhvn@gmail.com>
2021-03-09 23:26:29 +0700

    asdasd

--- app/Models/Logging.php
+++ app/Models/Logging.php
@@ -19,22 +19,6 @@ class Logging
     {
         Logging::log3($param1, $param2, "");
     }
-
-    public static function logapi($param1){
-        $name = "";
-        $user = auth('api')->user();
-        if ($user != null) {
-            if ($user->id != null)
-                $name = DB::table('users')->where('id', '=', $user->id)->get()->first()->name;
-        }
-
-        $values = array('user' => $name,
-            'param1' => $param1, 'param2' => "", 'param3' => "", 'param4' => 'api', 'param5' => $_SERVER['REMOTE_ADDR'],
-            'updated_at' => new \DateTime());
-        $values['created_at'] = new \DateTime();
-        DB::table('logging')->insert($values);
-        
-    }
         
     public static function log3($param1, $param2, $param3)
     {
