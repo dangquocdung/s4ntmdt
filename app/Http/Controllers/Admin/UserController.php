781ae2201f801e47730c552ccebde7e058b6d35d
dangquocdung <dungthinhvn@gmail.com>
2021-03-09 17:37:10 +0700

    asdsa

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -465,9 +465,9 @@ class UserController extends Controller
 				
     if($search_val && $search_val != ''){
       $getuserdata = User::where($where)->where('deleted_at', null)
-                     ->where('name', 'LIKE', $search_val.'%')  
-                     ->orWhere('display_name', 'LIKE', $search_val.'%')  
-                     ->orWhere('email', 'LIKE', $search_val.'%')  
+                     ->where('name', 'LIKE', '%'.$search_val.'%')  
+                     ->orWhere('display_name', 'LIKE', '%'.$search_val.'%')  
+                     ->orWhere('email', 'LIKE', '%'.$search_val.'%')  
                      ->orderBy('id','desc')
                      ->get();
     }
