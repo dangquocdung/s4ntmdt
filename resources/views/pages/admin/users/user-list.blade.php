331b2f339cc3ff21a0f77e821a80191646e4808f
dangquocdung <dungthinhvn@gmail.com>
2021-03-09 18:29:56 +0700

    asdasd

--- resources/views/pages/admin/users/user-list.blade.php
+++ resources/views/pages/admin/users/user-list.blade.php
@@ -115,9 +115,26 @@
 
 $(document).ready( function () {
     $('#user-list-tbl').DataTable({
-      language: {
-        url: '//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json'
-    }
+      "language": {
+        "sProcessing": "Traitement en cours ...",
+        "sLengthMenu": "Afficher _MENU_ lignes",
+        "sZeroRecords": "Aucun résultat trouvé",
+        "sEmptyTable": "Aucune donnée disponible",
+        "sInfo": "Lignes _START_ à _END_ sur _TOTAL_",
+        "sInfoEmpty": "Aucune ligne affichée",
+        "sInfoFiltered": "(Filtrer un maximum de_MAX_)",
+        "sInfoPostFix": "",
+        "sSearch": "Chercher:",
+        "sUrl": "",
+        "sInfoThousands": ",",
+        "sLoadingRecords": "Chargement...",
+        "oPaginate": {
+          "sFirst": "Premier", "sLast": "Dernier", "sNext": "Suivant", "sPrevious": "Précédent"
+        },
+        "oAria": {
+          "sSortAscending": ": Trier par ordre croissant", "sSortDescending": ": Trier par ordre décroissant"
+        }
+      }
     });
 } );
 
